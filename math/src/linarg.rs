ee8ac74f3288850b464a0aee8d242eeaf8bdaddd
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-10-03 00:03:22 +0900

    fix: compile errors, add: some tests

--- math/src/linarg.rs
+++ math/src/linarg.rs
@@ -1,6 +1,7 @@
 //! PeridotExtendedMathematics: Vector/Matrix
 
 use numtraits::{One, Zero};
+use std::ops::{Mul, Div, Add};
 
 /// 2-dimensional vector
 #[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Vector2<T>(pub T, pub T);
@@ -23,9 +24,9 @@ pub type QuaternionF32 = Quaternion<f32>;
 /// 2x2 matrix
 #[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Matrix2<T>(pub [T; 2], pub [T; 2]);
 /// 3x3 matrix
-#[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Matrix3<T>(pub [T; 3], pub [T; 3]);
+#[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Matrix3<T>(pub [T; 3], pub [T; 3], pub [T; 3]);
 /// 4x4 matrix
-#[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Matrix4<T>(pub [T; 4], pub [T; 4]);
+#[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Matrix4<T>(pub [T; 4], pub [T; 4], pub [T; 4], pub [T; 4]);
 /// 2x3 matrix
 #[derive(Debug, Clone, PartialEq, Eq)] #[repr(C)] pub struct Matrix2x3<T>(pub [T; 3], pub [T; 3]);
 /// 3x4 matrix
@@ -60,43 +61,43 @@ impl<T> Vector4<T> {
 
 // Extending/Shrinking Vector Types //
 impl<T: One> From<Vector2<T>> for Vector3<T> {
-    fn from(Vector2(x, y): Vector2) -> Self { Vector3(x, y, T::ONE) }
+    fn from(Vector2(x, y): Vector2<T>) -> Self { Vector3(x, y, T::ONE) }
 }
 impl<T: One> From<Vector3<T>> for Vector4<T> {
-    fn from(Vector3(x, y, z): Vector3) -> Self { Vector4(x, y, z, T::ONE) }
+    fn from(Vector3(x, y, z): Vector3<T>) -> Self { Vector4(x, y, z, T::ONE) }
 }
-impl<T: Div<T>> From<Vector4<T>> for Vector3<<T as Div>::Output> {
-    fn from(Vector4(x, y, z, w): Vector4) -> Self { Vector3(x / w, y / w, z / w) }
+impl<T: Div<T> + Copy> From<Vector4<T>> for Vector3<<T as Div>::Output> {
+    fn from(Vector4(x, y, z, w): Vector4<T>) -> Self { Vector3(x / w, y / w, z / w) }
 }
 
 // Vector as Fixed Arrays //
-impl<T> From<Vector2<T>> for [T; 2] { fn from(Vector2(x, y): Vector2) -> Self { [x, y] } }
-impl<T> From<Vector3<T>> for [T; 3] { fn from(Vector3(x, y, z): Vector3) -> Self { [x, y, z] } }
-impl<T> From<Vector4<T>> for [T; 4] { fn from(Vector4(x, y, z, w): Vector4) -> Self { [x, y, z, w] } }
+impl<T> Into<[T; 2]> for Vector2<T> { fn into(self) -> [T; 2] { [self.0, self.1] } }
+impl<T> Into<[T; 3]> for Vector3<T> { fn into(self) -> [T; 3] { [self.0, self.1, self.2] } }
+impl<T> Into<[T; 4]> for Vector4<T> { fn into(self) -> [T; 4] { [self.0, self.1, self.2, self.3] } }
 
 // Identities(for Multiplication) //
 impl<T: Zero + One> One for Matrix2<T> {
     const ONE: Self = Matrix2([T::ONE, T::ZERO], [T::ZERO, T::ONE]);
 }
 impl<T: Zero + One> One for Matrix3<T> {
-    const ONE: Self = Matrix3([T::ONE, T::ZERO: T::ZERO], [T::ZERO, T::ONE, T::ZERO], [T::ZERO, T::ZERO, T::ONE]);
+    const ONE: Self = Matrix3([T::ONE, T::ZERO, T::ZERO], [T::ZERO, T::ONE, T::ZERO], [T::ZERO, T::ZERO, T::ONE]);
 }
 impl<T: Zero + One> One for Matrix4<T> {
     const ONE: Self = Matrix4(
-        [T::ONE, T::ZERO: T::ZERO, T::ZERO], [T::ZERO, T::ONE, T::ZERO, T::ZERO],
+        [T::ONE, T::ZERO, T::ZERO, T::ZERO], [T::ZERO, T::ONE, T::ZERO, T::ZERO],
         [T::ZERO, T::ZERO, T::ONE, T::ZERO], [T::ZERO, T::ZERO, T::ZERO, T::ONE]);
 }
 impl<T: Zero + One> One for Matrix2x3<T> {
     const ONE: Self = Matrix2x3([T::ONE, T::ZERO, T::ZERO], [T::ZERO, T::ONE, T::ZERO]);
 }
 impl<T: Zero + One> One for Matrix3x4<T> {
-    const ONE: Self = Matrix3x4([T::ONE, T::ZERO: T::ZERO, T::ZERO],
+    const ONE: Self = Matrix3x4([T::ONE, T::ZERO, T::ZERO, T::ZERO],
         [T::ZERO, T::ONE, T::ZERO, T::ZERO], [T::ZERO, T::ZERO, T::ONE, T::ZERO]);
 }
 
 // Matrix and Matrix Multiplication //
-impl<T> Mul for Matrix2<T> where T: Mul<T>, <T as Mul>::Output: Add {
-    type Output = Matirx2<<<T as Mul>::Output as Add>::Output>;
+impl<T: Copy> Mul for Matrix2<T> where T: Mul<T>, <T as Mul>::Output: Add {
+    type Output = Matrix2<<<T as Mul>::Output as Add>::Output>;
     fn mul(self, other: Matrix2<T>) -> Self::Output {
         let dp = |src: &[T; 2], colindex: usize| {
             src[0] * other.0[colindex] + src[1] * other.1[colindex]
@@ -105,8 +106,8 @@ impl<T> Mul for Matrix2<T> where T: Mul<T>, <T as Mul>::Output: Add {
         Matrix2([dp(&self.0, 0), dp(&self.0, 1)], [dp(&self.1, 0), dp(&self.1, 1)])
     }
 }
-impl<T> Mul for Matrix3<T> where T: Mul<T>, <T as Mul>::Output: Add {
-    type Output = Matirx3<<<T as Mul>::Output as Add>::Output>;
+impl<T: Copy> Mul for Matrix3<T> where T: Mul<T>, <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
+    type Output = Matrix3<<T as Mul>::Output>;
     fn mul(self, other: Matrix3<T>) -> Self::Output {
         let dp = |src: &[T; 3], colindex: usize| {
             src[0] * other.0[colindex] + src[1] * other.1[colindex] + src[2] * other.2[colindex]
@@ -116,8 +117,8 @@ impl<T> Mul for Matrix3<T> where T: Mul<T>, <T as Mul>::Output: Add {
             [dp(&self.1, 0), dp(&self.1, 1), dp(&self.1, 2)], [dp(&self.2, 0), dp(&self.2, 1), dp(&self.2, 2)])
     }
 }
-impl<T> Mul for Matrix4<T> where T: Mul<T>, <T as Mul>::Output: Add {
-    type Output = Matirx4<<<T as Mul>::Output as Add>::Output>;
+impl<T: Copy> Mul for Matrix4<T> where T: Mul<T>, <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
+    type Output = Matrix4<<T as Mul>::Output>;
     fn mul(self, other: Matrix4<T>) -> Self::Output {
         let dp = |src: &[T; 4], colindex: usize| {
             src[0] * other.0[colindex] + src[1] * other.1[colindex] +
@@ -132,13 +133,13 @@ impl<T> Mul for Matrix4<T> where T: Mul<T>, <T as Mul>::Output: Add {
 }
 
 // Scaling, Rotating //
-impl<T> Matrix2<T> {
-    pub fn scale(self, v: Vector2) -> Self {
-        Matrix2([self.0[0] * v.x, self.0[1] * v.x], [self.1[0] * v.y, self.1[1] * v.y])
+impl<T: Copy> Matrix2<T> where T: Mul<Output = T> {
+    pub fn scale(self, v: Vector2<T>) -> Self {
+        Matrix2([self.0[0] * v.0, self.0[1] * v.0], [self.1[0] * v.1, self.1[1] * v.1])
     }
 }
 // Rotating //
-impl<T> Matrix2<T> where T: Mul<f32, Output = T> {
+impl Matrix2<f32> {
     pub fn rotate(self, rad: f32) -> Self {
         let (s, c) = rad.sin_cos();
         /*
@@ -151,15 +152,15 @@ impl<T> Matrix2<T> where T: Mul<f32, Output = T> {
         )
     }
 }
-impl<T> Matrix3<T> {
-    pub fn scale(self, v: Vector3) -> Self {
-        Matrix3([self.0[0] * v.x, self.0[1] * v.x, self.0[2] * v.x],
-            [self.1[0] * v.y, self.1[1] * v.y, self.2[2] * v.y],
-            [self.2[0] * v.z, self.2[1] * v.z, self.2[2] * v.z])
+impl<T: Copy> Matrix3<T> where T: Mul<Output = T> {
+    pub fn scale(self, v: Vector3<T>) -> Self {
+        Matrix3([self.0[0] * v.0, self.0[1] * v.0, self.0[2] * v.0],
+            [self.1[0] * v.1, self.1[1] * v.1, self.2[2] * v.1],
+            [self.2[0] * v.2, self.2[1] * v.2, self.2[2] * v.2])
     }
 }
-impl<T> Matrix4<T> {
-    pub fn scale(self, v: Vector3) -> Self {
+impl<T: Copy> Matrix4<T> where T: Mul<Output = T> {
+    pub fn scale(self, v: Vector4<T>) -> Self {
         Matrix4([self.0[0] * v.0, self.0[1] * v.0, self.0[2] * v.0, self.0[3] * v.0],
             [self.1[0] * v.1, self.1[1] * v.1, self.2[2] * v.1, self.1[3] * v.1],
             [self.2[0] * v.2, self.2[1] * v.2, self.2[2] * v.2, self.2[3] * v.2],
@@ -167,68 +168,68 @@ impl<T> Matrix4<T> {
     }
 }
 
-fn dotproduct2<T: Mul>(a: &[T; 2], b: &[T; 2]) -> <T as Mul>::Output
-        where <T as Mul>::Output: Add<<T as Mul>::Output, Output = <T as Mul>::Output> {
+fn dotproduct2<T: Mul + Copy>(a: &[T; 2], b: &[T; 2]) -> <T as Mul>::Output
+        where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     a[0] * b[0] + a[1] * b[1]
 }
-fn dotproduct3<T: Mul>(a: &[T; 3], b: &[T; 3]) -> <T as Mul>::Output
-        where <T as Mul>::Output: Add<<T as Mul>::Output, Output = <T as Mul>::Output> {
+fn dotproduct3<T: Mul + Copy>(a: &[T; 3], b: &[T; 3]) -> <T as Mul>::Output
+        where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
 }
-fn dotproduct4<T: Mul>(a: &[T; 4], b: &[T; 4]) -> <T as Mul>::Output
-        where <T as Mul>::Output: Add<<T as Mul>::Output, Output = <T as Mul>::Output> {
+fn dotproduct4<T: Mul + Copy>(a: &[T; 4], b: &[T; 4]) -> <T as Mul>::Output
+        where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3]
 }
 
 // Point Translation by Multiplication //
-impl<T: Mul> Mul<Vector2<T>> for Matrix2<T> {
+impl<T: Mul + Copy> Mul<Vector2<T>> for Matrix2<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     type Output = Vector2<<T as Mul>::Output>;
-    fn mul(self, v: Vector2) -> Self::Output {
+    fn mul(self, v: Vector2<T>) -> Self::Output {
         let va = v.into();
         Vector2(dotproduct2(&va, &self.0), dotproduct2(&va, &self.1))
     }
 }
-impl<T: Mul> Mul<Vector3<T>> for Matrix3<T> {
+impl<T: Mul + Copy> Mul<Vector3<T>> for Matrix3<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     type Output = Vector3<<T as Mul>::Output>;
-    fn mul(self, v: Vector3) -> Self::Output {
+    fn mul(self, v: Vector3<T>) -> Self::Output {
         let va = v.into();
         Vector3(dotproduct3(&va, &self.0), dotproduct3(&va, &self.1), dotproduct3(&va, &self.2))
     }
 }
-impl<T: Mul> Mul<Vector4<T>> for Matrix4<T> {
+impl<T: Mul + Copy> Mul<Vector4<T>> for Matrix4<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     type Output = Vector4<<T as Mul>::Output>;
-    fn mul(self, v: Vector4) -> Self::Output {
+    fn mul(self, v: Vector4<T>) -> Self::Output {
         let va = v.into();
         Vector4(dotproduct4(&va, &self.0), dotproduct4(&va, &self.1),
             dotproduct4(&va, &self.2), dotproduct4(&va, &self.3))
     }
 }
-impl<T: Mul> Mul<Vector2<T>> for Matrix2x3<T> {
+impl<T: Mul + One + Copy> Mul<Vector2<T>> for Matrix2x3<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     type Output = Vector2<<T as Mul>::Output>;
-    fn mul(self, v: Vector2) -> Self::Output {
+    fn mul(self, v: Vector2<T>) -> Self::Output {
         let va = Vector3::from(v).into();
         Vector2(dotproduct3(&va, &self.0), dotproduct3(&va, &self.1))
     }
 }
-impl<T: Mul> Mul<Vector3<T>> for Matrix3x4<T> {
+impl<T: Mul + One + Copy> Mul<Vector3<T>> for Matrix3x4<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
     type Output = Vector3<<T as Mul>::Output>;
-    fn mul(self, v: Vector3) -> Self::Output {
+    fn mul(self, v: Vector3<T>) -> Self::Output {
         let va = Vector4::from(v).into();
-        Vector2(dotproduct4(&va, &self.0), dotproduct4(&va, &self.1))
+        Vector3(dotproduct4(&va, &self.0), dotproduct4(&va, &self.1), dotproduct4(&va, &self.2))
     }
 }
 // shortcuts //
-impl<T: Mul> Mul<Vector2<T>> for Matrix3<T> {
-    type Output = <Matrix3 as Mul<Vector3<T>>>::Output;
-    fn mul(self, v: Vector2) -> Self::Output { self * Vector3::from(v) }
+impl<T: Mul + One + Copy> Mul<Vector2<T>> for Matrix3<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
+    type Output = <Matrix3<T> as Mul<Vector3<T>>>::Output;
+    fn mul(self, v: Vector2<T>) -> Self::Output { self * Vector3::from(v) }
 }
-impl<T: Mul> Mul<Vector2<T>> for Matrix4<T> {
-    type Output = <Matrix4 as Mul<Vector4<T>>>::Output;
-    fn mul(self, v: Vector2) -> Self::Output { self * Vector3::from(v) }
+impl<T: Mul + One + Copy> Mul<Vector2<T>> for Matrix4<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
+    type Output = <Matrix4<T> as Mul<Vector4<T>>>::Output;
+    fn mul(self, v: Vector2<T>) -> Self::Output { self * Vector3::from(v) }
 }
-impl<T: Mul> Mul<Vector3<T>> for Matrix4<T> {
-    type Output = <Matrix4 as Mul<Vector4<T>>>::Output;
-    fn mul(self, v: Vector3) -> Self::Output { self * Vector4::from(v) }
+impl<T: Mul + One + Copy> Mul<Vector3<T>> for Matrix4<T> where <T as Mul>::Output: Add<Output = <T as Mul>::Output> {
+    type Output = <Matrix4<T> as Mul<Vector4<T>>>::Output;
+    fn mul(self, v: Vector3<T>) -> Self::Output { self * Vector4::from(v) }
 }
 
 // Length Function and Normalization //
@@ -282,11 +283,26 @@ impl Vector4<f64> {
 }
 
 // quaternion shortcuts //
-impl<T> Quaternion<T> where T: Mul<Output = T> {
-    pub fn new(rad: f32, axis: Vector3<T>) -> Self {
+impl Quaternion<f32> {
+    pub fn new(rad: f32, axis: Vector3<f32>) -> Self {
         let (s, c) = (rad / 2.0).sin_cos();
         let axis = axis.normalize();
 
         Quaternion(axis.0 * s, axis.1 * s, axis.2 * s, c)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] fn vector_dimension_transform() {
+        assert_eq!(Vector3::from(Vector2(2, 3)), Vector3(2, 3, 1));
+        assert_eq!(Vector4::from(Vector3(2.5, 3.0, 4.1)), Vector4(2.5, 3.0, 4.1, 1.0));
+        assert_eq!(Vector3::from(Vector4(4, 6, 8, 2)), Vector3(2, 3, 4));
+    }
+    #[test] fn matrix_translation() {
+        assert_eq!(Matrix3([1, 0, 2], [0, 1, 3], [0, 0, 1]) * Vector2(0, 0), Vector3(2, 3, 1));
+        assert_eq!(Matrix4([1, 0, 0, 2], [0, 1, 0, 3], [0, 0, 1, 4], [0, 0, 0, 1]) * Vector3(1, 2, 3), Vector4(3, 5, 7, 1));
     }
 }
