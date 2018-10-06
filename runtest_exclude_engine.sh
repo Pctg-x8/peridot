0f3f041acd8d25f4ad3777177e890bb60ec8e104
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-10-06 17:03:01 +0900

    add: test runner exclude engine

---
+++ runtest_exclude_engine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+shopt -s extglob
+
+# for first branch
+for c in */Cargo.toml
+do (cd $(dirname $c) && cargo test --verbose) || exit 1; done
+
+# for second branch
+for c in !(extras)/*/Cargo.toml
+do (cd $(dirname $c) && cargo test --verbose) || exit 2; done
+
