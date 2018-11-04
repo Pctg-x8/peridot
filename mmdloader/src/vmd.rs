8dd39a583cc3e7dac9b92023ea5fd7fe97282adb
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-11-04 18:01:09 +0900

    add: framecount in motiondata

--- mmdloader/src/vmd.rs
+++ mmdloader/src/vmd.rs
@@ -7,7 +7,7 @@ use encoding::codec::japanese::Windows31JEncoding;
 use encoding::{Encoding, DecoderTrap};
 
 pub struct MotionData {
-    pub name_cell: String
+    pub name_cell: String, pub framecount: u32
 }
 impl MotionData {
     pub fn read<R: Read>(reader: &mut R) -> Result<Self, LoadingError> {
@@ -16,12 +16,11 @@ impl MotionData {
         if &header_bytes[..30] != b"Vocaloid Motion Data 0002\0\0\0\0\0" {
             return Err(LoadingError::SignatureMismatch);
         }
-        let fc = unsafe { transmute::<_, &[u32]>(&header_bytes[50..])[0] };
-        println!("FrameCount: {}", fc);
+        let framecount = unsafe { transmute::<_, &[u32]>(&header_bytes[50..])[0] };
         let name_bytecount = header_bytes[30..50].iter().position(|&b| b == 0x00).unwrap_or(20);
         let name = Windows31JEncoding.decode(&header_bytes[30..30 + name_bytecount], DecoderTrap::Strict)
             .expect("Invalid Shift-JIS Sequence in name");
-        return Ok(MotionData { name });
+        return Ok(MotionData { name, framecount });
     }
 }
 
