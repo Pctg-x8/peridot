4f6c9629acfe6f1dac78f9e18368a9bd6153e2a1
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-11-07 10:02:23 +0900

    add: projection matrix

--- math/src/gaming.rs
+++ math/src/gaming.rs
@@ -1,6 +1,5 @@
 //! Peridot Extended Mathematics: Gaming Utils(Camera, ModelMatrix)
 
-use numtraits::*;
 use linarg::*;
 use std::ops::{Range, Neg};
 
@@ -11,18 +10,33 @@ pub struct Camera {
 }
 
 impl Camera {
+    /// calculates the camera projection matrix
     pub fn projection_matrix(&self) -> Matrix4F32 {
         match self.projection {
             ProjectionMethod::Perspective { fov } => {
-                Matrix4::ONE
+                let scaling = (fov / 2.0).tan().recip();
+                let zdiff = self.depth_range.end - self.depth_range.start;
+                let zscale = (-self.depth_range.end / zdiff, -(self.depth_range.end * self.depth_range.start) / zdiff);
+                
+                Matrix4([scaling, 0.0, 0.0, 0.0], [0.0, scaling, 0.0, 0.0],
+                    [0.0, 0.0, zscale.0, -1.0], [0.0, 0.0, zscale.1, 0.0])
             },
             ProjectionMethod::Orthographic { size } => {
-                Matrix4::ONE
+                let zdiff = self.depth_range.end - self.depth_range.start;
+                let t = Matrix4::translation(Vector3(0.0, 0.0, -self.depth_range.start));
+                let s = Matrix4::scale(Vector4(size.recip(), size.recip(), zdiff.recip(), 1.0));
+
+                t * s
             }
         }
     }
+    /// calculates the camera view matrix
     pub fn view_matrix(&self) -> Matrix4F32 {
         Matrix4F32::translation(-self.position.clone()) * Matrix4F32::from(self.rotation.clone().neg())
+    }
+    /// calculates the camera view matrix and the projection matrix(returns in this order)
+    pub fn matrixes(&self) -> (Matrix4F32, Matrix4F32) {
+        (self.view_matrix(), self.projection_matrix())
     }
 }
 
