1ace66e81e3fb5a2edd36eecfce4617fd35a18af
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-11-08 10:34:04 +0900

    fix output

--- vulnerabilities_elliminator.py
+++ vulnerabilities_elliminator.py
@@ -7,12 +7,13 @@ found_unwrap_vulnerabilities = []
 for fpath in filter(lambda p: not p.startswith("extras/"), iglob("**/*.rs", recursive=True)):
     with open(fpath) as fp:
         for i, line in filter(lambda x: not pat_comment_line.match(x[1]), enumerate(iter(fp.readline, ""), 1)):
-            if "unwrap()" in line: found_unwrap_vulnerabilities.append((fpath, i, line.replace("unwrap", "\033[1;31munwrap\033[0m")))
+            if "unwrap()" in line:
+                found_unwrap_vulnerabilities.append((fpath, i, line.replace("unwrap", "\033[1;31munwrap\033[0m")))
 
 if not found_unwrap_vulnerabilities:
     print("No unwraps found")
 else:
     for fpath, line, text in found_unwrap_vulnerabilities:
-        print(f"Unwrap Found! Use `expect` with message instead: {fpath}:{line}")
+        print(f"Unwrap Found! Use `expect` with message instead: \033[1m{fpath}:{line}\033[0m")
         print(text.rstrip())
     exit(1)
