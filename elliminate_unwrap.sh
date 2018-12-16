951317f2a2d819a55006973262ddeb9f0840dd3c
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-12-16 20:27:16 +0900

    strip unused

--- elliminate_unwrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-IFS=$'\n' LINES=`grep unwrap **/*.rs -n | sed -e '/:[ \f\n\t\r]*\/\//d' -e 's/^\([^:]*\):\([0-9]*\):\(.*\)/Unwrap Found! Use expect with message instead. => \1:\2\\\x0a\3/p'`
-if [[ ! -z $LINES ]]; then (echo $LINES; exit 1); else echo "No unwraps found"; fi
