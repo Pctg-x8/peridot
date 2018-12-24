a31fd8e22409fc32b7b4a11286edf366cd7ff8b2
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-12-25 00:26:46 +0900

    <~dev

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ./build.sh に続けて,区切りでcradleの名前を指定する
+
+TARGETS=$(tr -s ',' ' ' <<< "$1")
+shift
+
+for t in $TARGETS; do
+    echo "Building for target '$t'..."
+    ./cradle/$t/build.sh $@
+#    Invoke-Expression "./cradle/$Target/build.ps1 $PassingArgs"
+done
---
+++ cradle/android/app/build/generated/source/buildConfig/debug/com/cterm2/infinitesweeper/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cterm2.infinitesweeper;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cterm2.infinitesweeper";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 0;
+  public static final String VERSION_NAME = "0.1.0";
+}
