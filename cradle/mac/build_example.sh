82bd4aa1cdb09e9bd387740209823478063eaadf
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-01-02 21:31:59 +0900

    add: build script

--- cradle/mac/build_example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cargo build --features bedrock/VK_EXT_debug_report
-install_name_tool -change @rpath/vulkan.framework/Versions/A/vulkan @executable_path/vulkan.framework/Versions/A/vulkan ../../target/debug/peridot-cradle-mac
-cp -r $VK_SDK_PATH/macOS/Frameworks/vulkan.framework ../../target/debug/
-eval ../../target/debug/peridot-cradle-mac
