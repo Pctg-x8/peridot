b5d8da945b6e2f0d2405b682cc639a8159c141e2
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-01-05 13:40:03 +0900

    remove pluginloader

--- src/lib.rs
+++ src/lib.rs
@@ -27,38 +27,18 @@ pub mod utils; pub use self::utils::*;
 mod asset; pub use self::asset::*;
 mod input; pub use self::input::*;
 
-pub trait PluginLoader {
+pub trait NativeLinker {
     type AssetLoader: PlatformAssetLoader;
+    type RenderTargetProvider: PlatformRenderTarget;
     type InputProcessor: InputProcessPlugin;
-    type RenderTargetProvider: PlatformRenderTarget;
-
-    fn new_asset_loader(&self) -> Self::AssetLoader;
-    fn new_render_target_provider(&self) -> Self::RenderTargetProvider;
-    fn input_processor(&mut self) -> &mut Self::InputProcessor;
-}
-pub struct NativeLink<AL: PlatformAssetLoader, PRT: PlatformRenderTarget> {
-    prt: PRT, asset_loader: AL
-}
-pub trait PlatformLinker {
-    type AssetLoader: PlatformAssetLoader;
-    type RenderTargetProvider: PlatformRenderTarget;
 
     fn new(al: Self::AssetLoader, prt: Self::RenderTargetProvider) -> Self;
     fn asset_loader(&self) -> &Self::AssetLoader;
     fn render_target_provider(&self) -> &Self::RenderTargetProvider;
-}
-impl<AL: PlatformAssetLoader, PRT: PlatformRenderTarget> PlatformLinker for NativeLink<AL, PRT> {
-    type AssetLoader = AL;
-    type RenderTargetProvider = PRT;
-
-    fn new(al: AL, prt: PRT) -> Self {
-        NativeLink { asset_loader: al, prt }
-    }
-    fn asset_loader(&self) -> &AL { &self.asset_loader }
-    fn render_target_provider(&self) -> &PRT { &self.prt }
+    fn input_processor_mut(&mut self) -> &mut Self::InputProcessor;
 }
 
-pub trait EngineEvents<PL: PlatformLinker> : Sized {
+pub trait EngineEvents<PL: NativeLinker> : Sized {
     fn init(_e: &Engine<Self, PL>) -> Self;
     /// Updates the game and passes copying(optional) and rendering command batches to the engine.
     fn update(&mut self, _e: &Engine<Self, PL>, _on_backbuffer_of: u32)
@@ -71,18 +51,16 @@ pub trait EngineEvents<PL: PlatformLinker> : Sized {
     /// (called after discard_backbuffer_resources so re-create discarded resources here)
     fn on_resize(&mut self, _e: &Engine<Self, PL>, _new_size: math::Vector2<usize>) {}
 }
-impl<PL: PlatformLinker> EngineEvents<PL> for () {
+impl<PL: NativeLinker> EngineEvents<PL> for () {
     fn init(_e: &Engine<Self, PL>) -> Self { () }
 }
 
-pub struct Engine<E: EngineEvents<PL>, PL: PlatformLinker> {
+pub struct Engine<E: EngineEvents<PL>, PL: NativeLinker> {
     nativelink: PL, surface: SurfaceInfo, wrt: Discardable<WindowRenderTargets>,
     pub(self) g: Graphics, event_handler: Option<RefCell<E>>, ip: Rc<InputProcess>
 }
-impl<E: EngineEvents<NPL>, NPL: PlatformLinker> Engine<E, NPL> {
-    pub fn launch<PL>(name: &str, version: (u32, u32, u32), plugin_loader: &mut PL) -> br::Result<Self>
-            where PL: PluginLoader<AssetLoader=NPL::AssetLoader, RenderTargetProvider=NPL::RenderTargetProvider> {
-        let nativelink = NPL::new(plugin_loader.new_asset_loader(), plugin_loader.new_render_target_provider());
+impl<E: EngineEvents<PL>, PL: NativeLinker> Engine<E, PL> {
+    pub fn launch<PL>(name: &str, version: (u32, u32, u32), nativelink: PL) -> br::Result<Self> {
         let g = Graphics::new(name, version, nativelink.render_target_provider().surface_extension_name())?;
         let surface = nativelink.render_target_provider().create_surface(&g.instance, &g.adapter,
             g.graphics_queue.family)?;
@@ -96,7 +74,7 @@ impl<E: EngineEvents<NPL>, NPL: PlatformLinker> Engine<E, NPL> {
         this.submit_commands(|r| this.wrt.get().emit_initialize_backbuffers_commands(r))
             .expect("Initializing Backbuffers");
         this.event_handler = Some(eh.into());
-        plugin_loader.input_processor().on_start_handle(&this.ip);
+        this.nativelink.input_processor_mut().on_start_handle(&this.ip);
         return Ok(this);
     }
     fn userlib_mut(&self) -> RefMut<E> { self.event_handler.as_ref().expect("uninitialized userlib").borrow_mut() }
@@ -145,8 +123,7 @@ impl<E: EngineEvents<NPL>, NPL: PlatformLinker> Engine<E, NPL> {
         {
             let bound_wrt = self.wrt.get();
 
-            let mut eh_mut = self.event_handler.as_ref().expect("uninitialized").borrow_mut();
-            let (copy_submission, mut fb_submission) = eh_mut.update(self, bb_index);
+            let (copy_submission, mut fb_submission) = self.userlib_mut().update(self, bb_index);
             if let Some(mut cs) = copy_submission {
                 // copy -> render
                 cs.signal_semaphores.to_mut().push(&self.g.buffer_ready);
@@ -183,10 +160,10 @@ impl<E: EngineEvents<NPL>, NPL: PlatformLinker> Engine<E, NPL> {
             .expect("Recreating WindowRenderTargets"));
         self.submit_commands(|r| self.wrt.get().emit_initialize_backbuffers_commands(r))
             .expect("Initializing Backbuffers");
-        self.userlib_mut().on_resize(&self, new_size);
+        self.userlib_mut().on_resize(self, new_size);
     }
 }
-impl<E: EngineEvents<PL>, PL: PlatformLinker> Drop for Engine<E, PL> {
+impl<E: EngineEvents<PL>, PL: NativeLinker> Drop for Engine<E, PL> {
     fn drop(&mut self) {
         self.graphics().device.wait().expect("device error");
     }
