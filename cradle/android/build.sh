ee9444fa39c4deacb2b22321429e7aedbf4226f7
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-05-10 00:31:00 +0900

    android build changes

--- cradle/android/build.sh
+++ cradle/android/build.sh
@@ -10,13 +10,18 @@ echo "Syncing Game Kernel source..."
 rsync -aud --progress $1/src/ src/game
 [ -f src/main.rs ] && rm src/main.rs
 
-RUSTFLAGS="-C link-arg=--sysroot=$ANDROID_HOME\ndk-bundle\platforms\android-$ANDROID_NDK_PLATFORM_TARGET\arch-arm64"
+echo "Building for Target-API-Level $ANDROID_TARGET_API_LEVEL"
 if [ ! -d /tmp/peridot/android-toolchain ]; then
-	$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --arch=arm64 --platform=android-$ANDROID_NDK_PLATFORM_TARGET \
---install-dir=/tmp/peridot/android-toolchain --force
+	$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --arch=arm64 --platform=android-$ANDROID_TARGET_API_LEVEL \
+--install-dir=/tmp/peridot/android-toolchain --force --verbose
 fi
-export CC=/tmp/peridot/android-toolchain/bin/aarch64-linux-android-clang
-export AR=/tmp/peridot/android-toolchain/bin/aarch64-linux-android-ar
-cargo build --features bedrock/VK_EXT_debug_report,bedrock/VK_KHR_android_surface,bedrock/DynamicLoaded
+export RUSTFLAGS="-C link-arg=--sysroot=/tmp/peridot/android-toolchain/sysroot"
+export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android$ANDROID_TARGET_API_LEVEL-clang
+export CXX=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android$ANDROID_TARGET_API_LEVEL-clang++
+export LD=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ld
+export AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
+# export LD=/tmp/peridot/android-toolchain/bin/aarch64-linux-android-ld
+# export AR=/tmp/peridot/android-toolchain/bin/aarch64-linux-android-ar
+cargo build --features bedrock/VK_EXT_debug_report,bedrock/VK_KHR_android_surface,bedrock/DynamicLoaded --target aarch64-linux-android
 [ -d ../../../target/arm64-v8a-linux-android ] && rm -r ../../../target/arm64-v8a-linux-android
 mv ../../../target/aarch64-linux-android ../../../target/arm64-v8a-linux-android
--- cradle/android/env.ps1
+++ cradle/android/env.ps1
@@ -15,12 +15,13 @@ if (!(Test-Path -Path "android-build")) {
     python $NdkPath\build\tools\make_standalone_toolchain.py --arch=arm64 --api=$Env:ANDROID_NDK_PLATFORM_TARGET --install-dir=android-build
 }
 else { Write-Host "* Using existing android build environment" }
-# $Env:PATH = Append-Variable $Env:PATH ";" "$NdkPath\toolchains\aarch64-linux-android-4.9\prebuilt\windows-x86_64\bin"
-$Env:CC = "$(Get-Location)/android-build/bin/aarch64-linux-android-clang.cmd"
-$Env:CXX = "$(Get-Location)/android-build/bin/aarch64-linux-android-clang++.cmd"
-$Env:LD = "$(Get-Location)/android-build/bin/aarch64-linux-android-clang.cmd"
-$Env:AR = "$(Get-Location)/android-build/bin/aarch64-linux-android-ar.exe"
+$Env:PATH = Append-Variable $Env:PATH ";" "$(Get-Location)\android-build\bin"
+$Env:CC = "$(Get-Location)\android-build\bin\aarch64-linux-android-clang.cmd"
+$Env:CXX = "$(Get-Location)\android-build\bin\aarch64-linux-android-clang++.cmd"
+$Env:LD = "$(Get-Location)\android-build\bin\aarch64-linux-android-clang.cmd"
+$Env:AR = "$(Get-Location)\android-build\bin\aarch64-linux-android-ar.exe"
 # $Env:CFLAGS = Append-Variable $Env:CFLAGS " " "-I$NdkPath\sysroot\usr\include"
 # $Env:CFLAGS = Append-Variable $Env:CFLAGS " " "-I$NdkPath\sysroot\usr\include\aarch64-linux-android"
 # $Env:CFLAGS = Append-Variable $Env:CFLAGS " " "--sysroot=$NdkPath\platforms\android-$ANDROID_NDK_PLATFORM_TARGET\arch-arm64"
 # $Env:CFLAGS = Append-Variable $Env:CFLAGS " " "-target aarch64-none-linux-android"
+$Env:LDFLAGS = Append-Variable $Env:LDFLAGS " " "-L${Env:FREETYPE_LIB_PATH}"
