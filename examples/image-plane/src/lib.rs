2c7e7d12284b51b26007dacefe779a06471471e1
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-06-01 17:48:34 +0900

    fix: Quaternion::new receives degrees

--- examples/image-plane/src/lib.rs
+++ examples/image-plane/src/lib.rs
@@ -203,7 +203,7 @@ impl<PL: peridot::NativeLinker> peridot::EngineEvents<PL> for Game<PL>
     fn update(&mut self, e: &peridot::Engine<Self, PL>, on_backbuffer_of: u32, delta_time: Duration)
             -> (Option<br::SubmissionBatch>, br::SubmissionBatch) {
         let dtsec = delta_time.as_secs() as f32 + delta_time.subsec_micros() as f32 / 1000_0000.0;
-        self.rot += (dtsec * 60.0 * 1.0).to_radians();
+        self.rot += dtsec * 15.0;
         self.buffers.mut_buffer.0.guard_map(size_of::<Matrix4F32>() as _, |m| unsafe
         {
             m.get_mut::<Uniform>(self.mut_uniform_offset as _).object
