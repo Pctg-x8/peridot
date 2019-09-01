cbd1b600e3f8f89de2d50c519ab75db57ec2bfba
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-09-01 12:29:28 +0900

    fix: exclude testing shaderbuild

--- runtest_exclude_engine.sh
+++ runtest_exclude_engine.sh
@@ -7,10 +7,11 @@ for c in !(examples|vertex-processing-pack)/Cargo.toml
 do (cd $(dirname $c) && cargo test --verbose) || exit 1; done
 
 # for second branch
-for c in !(extras|cradle|examples)/*/Cargo.toml
+for c in !(extras|cradle|examples)/!(shaderbuild)/Cargo.toml
 do (cd $(dirname $c) && cargo test --verbose) || exit 2; done
 
 # check engine and other untested modules
+(cd tools/shaderbuild && cargo check --verbose)
 (cd vertex-processing-pack && cargo check --verbose)
 cargo check --verbose --features=bedrock/VK_EXT_debug_report
 
