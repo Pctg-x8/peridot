64b636d9cb4ebfca703f42be15ca94db113a95f1
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-09-01 10:01:27 +0900

    add: Module Interface

---
+++ mif/src/asset.rs
@@ -0,0 +1,27 @@
+//! Asset Interface
+
+use std::io::Error as IOError;
+
+/// Asset Data Description in Filesystem.
+pub trait LogicalAssetData: Sized
+{
+    /// Extension
+    const EXT: &'static str;
+}
+
+/// Construct Asset Object from Readers.
+pub trait FromAsset: LogicalAssetData
+{
+    /// Reading Error
+    type Error: From<IOError>;
+    /// Construct Asset from Reader
+    fn from_asset<Asset: Read + Seek + 'static>(asset: Asset) -> Result<Self, Self::Error>;
+}
+/// Construct Streaming Asset Object with Readers.
+pub trait FromStreamingAsset: LogicalAssetData
+{
+    /// Reading Error
+    type Error: From<IOError>;
+    /// Construct Streaming Asset with Reader.
+    fn from_asset<Asset: Read + 'static>(asset: Asset) -> Result<Self, Self::Error>;
+}
