c5e2920fd6b3e00ee2920bb6fd1654b462ad7db0
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-09-01 12:20:42 +0900

    fix: runner script

--- runtest_exclude_engine.sh
+++ runtest_exclude_engine.sh
@@ -3,13 +3,14 @@
 shopt -s extglob
 
 # for first branch
-for c in !(examples)/Cargo.toml
+for c in !(examples|vertex-processing-pack)/Cargo.toml
 do (cd $(dirname $c) && cargo test --verbose) || exit 1; done
 
 # for second branch
 for c in !(extras|cradle|examples)/*/Cargo.toml
 do (cd $(dirname $c) && cargo test --verbose) || exit 2; done
 
-# check engine
+# check engine and other untested modules
+(cd vertex-processing-pack && cargo check --verbose)
 cargo check --verbose --features=bedrock/VK_EXT_debug_report
 
