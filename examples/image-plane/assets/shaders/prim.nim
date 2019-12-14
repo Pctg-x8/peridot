6ecf3123f56cc42de93cb21cff8a91e726b559fd
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-12-14 12:17:37 +0900

    fix: nim shader code

--- examples/image-plane/assets/shaders/prim.nim
+++ examples/image-plane/assets/shaders/prim.nim
@@ -1,24 +1,25 @@
 import shaders
 
-type Camera = object
-  vp {.rowMajor.}: Matrix4x4
-  obj {.rowMajor.}: Matrix4x4
+type
+  Camera = object
+    vp {.rowMajor.}: Matrix4x4
+    obj {.rowMajor.}: Matrix4x4
 
 var
   camera {.uniform, descriptorSet: 0, binding: 0.}: Camera
   pos {.input, location: 0.}: Vector3
   uv {.input, location: 1.}: Vector2
-  raster_position {.output, buildIn: Position.}: Vector4
-  uv_v_out {.output: location: 0.}: Vector2
+  raster_position {.output, builtIn: Position.}: Vector4
+  uv_v_out {.output, location: 0.}: Vector2
 
   uv_v_in {.input, location: 0.}: Vector2
   sv_target {.output, location: 0.}: Vector4
-  tex {.uniform, descriptor: 0, binding: 1.}: Sampler2D
+
+proc project(p: Vector3): Vector4 = camera.vp * camera.obj * construct[Vector4](pos, 1.0'f32)
 
 proc vsMain() {.stage: Vertex.} =
-  raster_position = transpose(camera.vp) * transpose(camera.pos) * construct[Vector4](pos, 1.0'f32)
+  raster_position = project(pos)
   uv_v_out = uv
 
 proc fsMain() {.stage: Fragment.} =
-  sv_target = texture(tex, uv_v_out)
-
+  sv_target = construct[Vector4](uv_v_in, 0.0'f32, 1.0'f32)
