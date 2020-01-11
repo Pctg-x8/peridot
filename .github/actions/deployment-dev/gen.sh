c65323e75e455ef92dfb2c7d44a44d65bfd101af
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-01-11 19:09:00 +0900

    fix: use filesystem root for staging

--- .github/actions/deployment-dev/gen.sh
+++ .github/actions/deployment-dev/gen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-cd $GITHUB_WORKSPACE
-
 # Copy generated docs for deployment
-rsync --delete -auv ./target/doc/ ./doc/public/dev
-cd doc
+rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ /doc/public/dev
+cd /doc
 firebase deploy --token $FIREBASE_TOKEN --project docs-peridot
