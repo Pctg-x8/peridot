81efda0a94b07e5482c744ae9e23e004e2d99f3c
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-03-01 18:12:38 +0900

    add: asynchronous archive reader/writer

--- archive/src/lib.rs
+++ archive/src/lib.rs
@@ -1,29 +1,31 @@
 //! Peridot Archive
 
 use peridot_serialization_utils::*;
-use std::io::prelude::{Write, Read, BufRead};
+use async_std::io::prelude::{Write, Read, BufRead, ReadExt, SeekExt, WriteExt};
 use std::io::{Result as IOResult, Error as IOError};
-use std::io::{SeekFrom, Seek, BufReader};
-use std::fs::File;
+use async_std::io::{SeekFrom, Seek, BufReader, Cursor};
+use async_std::fs::File;
 use std::mem::{transmute, replace};
 use std::collections::HashMap;
 use libflate::deflate as zlib; use lz4_compression; use zstd;
 use crc::crc32;
-use std::path::Path;
+use async_std::path::Path;
+use async_std::task::{Context, Poll};
+use std::pin::Pin;
 
 #[repr(C)] pub struct LinearPaired2u64(u64, u64);
 #[derive(Debug)]
 #[repr(C)] pub struct AssetEntryHeadingPair { pub byte_length: u64, pub relative_offset: u64 }
 impl AssetEntryHeadingPair
 {
-    fn write<W: Write>(&self, writer: &mut W) -> IOResult<usize>
+    async fn write<W: Write + Unpin>(&self, writer: &mut W) -> IOResult<usize>
     {
-        writer.write(unsafe { &transmute::<_, &[u8; 8 * 2]>(self)[..] }).map(|_| 16)
+        writer.write(unsafe { &transmute::<_, &[u8; 8 * 2]>(self)[..] }).await.map(|_| 16)
     }
-    fn read<R: Read>(reader: &mut R) -> IOResult<Self>
+    async fn read<R: Read + Unpin>(reader: &mut R) -> IOResult<Self>
     {
         let mut sink = AssetEntryHeadingPair { byte_length: 0, relative_offset: 0 };
-        reader.read_exact(unsafe { &mut transmute::<_, &mut [u8; 8 * 2]>(&mut sink)[..] }).map(|_| sink)
+        reader.read_exact(unsafe { &mut transmute::<_, &mut [u8; 8 * 2]>(&mut sink)[..] }).await.map(|_| sink)
     }
 }
 
@@ -34,7 +36,6 @@ pub enum CompressionMethod
     None, Zlib(u64), Lz4(u64), Zstd11(u64)
 }
 
-use std::io::Cursor;
 pub struct ArchiveWrite(CompressionMethod, HashMap<String, AssetEntryHeadingPair>, Vec<u8>);
 impl ArchiveWrite
 {
@@ -55,142 +56,143 @@ impl ArchiveWrite
         true
     }
     /// return -> written bytes(raw)
-    fn write_asset_entries<W: Write>(&self, writer: &mut W) -> IOResult<usize>
+    async fn write_asset_entries<W: Write + Unpin>(&self, writer: &mut W) -> IOResult<usize>
     {
-        let mut written_bytes = VariableUInt(self.1.len() as _).write(writer)?;
+        let mut written_bytes = VariableUInt(self.1.len() as _).write(writer).await?;
         for (n, h) in &self.1
         {
-            written_bytes += h.write(writer).and_then(|w1| PascalStr(n).write(writer).map(move |w2| w1 + w2))?;
+            let wlen = h.write(writer).await?;
+            let swlen = PascalStr(n).write(writer).await?;
+            written_bytes += wlen + swlen;
         }
-        return Ok(written_bytes);
+        
+        Ok(written_bytes)
     }
-    pub fn write<W: Write>(&self, writer: &mut W) -> IOResult<()>
+    pub async fn write<W: Write + Unpin>(&self, writer: &mut W) -> IOResult<()>
     {
+        let mut body = Cursor::new(Vec::new());
+        let entry_bytes = self.write_asset_entries(&mut body).await?;
+        body.write_all(&self.2[..]).await?;
+        let uncompressed_bytes = entry_bytes + self.2.len();
+
         match self.0
         {
             CompressionMethod::None =>
             {
-                let mut body = Cursor::new(Vec::new());
-                self.write_asset_entries(&mut body)?; body.write_all(&self.2[..])?;
-
-                Self::write_common(writer, b"par ", None, &body.into_inner()[..])
+                Self::write_common(writer, b"par ", None, &body.into_inner()[..]).await
             },
-            CompressionMethod::Zlib(_) =>
+            CompressionMethod::Zlib(_) => async
             {
-                let mut body = zlib::Encoder::new(Cursor::new(Vec::new()));
-                let uncompressed_bytes = self.write_asset_entries(&mut body)
-                    .and_then(|wa| body.write_all(&self.2[..]).map(move |_| wa + self.2.len()))? as u64;
+                let encoded = async_std::task::spawn_blocking(||
+                {
+                    let mut encoder = zlib::Encoder::new(std::io::Cursor::new(Vec::new()));
+                    std::io::Write::write_all(&mut encoder, &body.into_inner())?;
+                    encoder.finish().into_result().map(|r| r.into_inner())
+                }).await?;
 
-                Self::write_common(writer, b"pard", Some(uncompressed_bytes),
-                    &body.finish().into_result()?.into_inner()[..])
-            }
-            CompressionMethod::Lz4(_) =>
+                Self::write_common(writer, b"pard", Some(uncompressed_bytes as u64), &encoded[..]).await
+            }.await,
+            CompressionMethod::Lz4(_) => async
             {
-                let mut body = Cursor::new(Vec::new());
-                let uncompressed_bytes = self.write_asset_entries(&mut body)
-                    .and_then(|wa| body.write_all(&self.2[..]).map(move |_| wa + self.2.len()))? as u64;
-                let body = lz4_compression::prelude::compress(&body.into_inner());
+                let encoded = lz4_compression::prelude::compress(&body.into_inner());
 
-                Self::write_common(writer, b"parz", Some(uncompressed_bytes), &body[..])
-            },
-            CompressionMethod::Zstd11(_) =>
+                Self::write_common(writer, b"parz", Some(uncompressed_bytes as u64), &encoded[..]).await
+            }.await,
+            CompressionMethod::Zstd11(_) => async
             {
-                let mut body = zstd::Encoder::new(Cursor::new(Vec::new()), 11)?;
-                let uncompressed_bytes = self.write_asset_entries(&mut body)
-                    .and_then(|wa| body.write_all(&self.2[..]).map(move |_| wa + self.2.len()))? as u64;
-                
-                Self::write_common(writer, b"par1", Some(uncompressed_bytes), &body.finish()?.into_inner()[..])
-            }
+                let encoded = async_std::task::spawn_blocking(
+                    || zstd::stream::encode_all(std::io::Cursor::new(body.into_inner()), 11)
+                ).await?;
+
+                Self::write_common(writer, b"par1", Some(uncompressed_bytes as u64), &encoded[..]).await
+            }.await
         }
     }
-    fn write_common<W: Write>(writer: &mut W, signature: &[u8], uncompressed_bytes: Option<u64>, body: &[u8])
+    async fn write_common<W: Write + Unpin>(writer: &mut W, signature: &[u8], uncompressed_bytes: Option<u64>, body: &[u8])
         -> IOResult<()>
     {
         let checksum = crc32::checksum_ieee(body);
-        writer.write_all(signature)?;
-        if let Some(ub) = uncompressed_bytes { writer.write_all(&ub.to_le_bytes())?; }
-        writer.write_all(&checksum.to_le_bytes())?;
-        writer.write_all(body)
+        writer.write_all(signature).await?;
+        if let Some(ub) = uncompressed_bytes { writer.write_all(&ub.to_le_bytes()).await?; }
+        writer.write_all(&checksum.to_le_bytes()).await?;
+        writer.write_all(body).await
     }
 }
 
-pub enum WhereArchive { OnMemory(Vec<u8>), FromIO(BufReader<File>) }
+enum WhereArchive { OnMemory(Vec<u8>), FromIO(BufReader<File>) }
 impl WhereArchive
 {
-    pub fn on_memory(&mut self) -> IOResult<&[u8]>
+    async fn on_memory(&mut self) -> IOResult<&[u8]>
     {
         let replace_buf = if let WhereArchive::FromIO(ref mut r) = self
         {
             let mut buf = Vec::new();
-            r.read_to_end(&mut buf)?; Some(buf)
+            r.read_to_end(&mut buf).await?;
+            Some(buf)
         }
         else { None };
         if let Some(b) = replace_buf { replace(self, WhereArchive::OnMemory(b)); }
-        match self
-        {
-            WhereArchive::OnMemory(ref b) => Ok(b),
-            _ => unreachable!()
-        }
+
+        Ok(self.ref_onmemory_buffer())
+    }
+    fn ref_onmemory_buffer(&self) -> &[u8]
+    {
+        if let WhereArchive::OnMemory(ref b) = self { b } else { unreachable!(); }
     }
 }
-pub enum EitherArchiveReader { OnMemory(Cursor<Vec<u8>>), FromIO(BufReader<File>) }
-impl EitherArchiveReader
+enum EitherArchiveReader
 {
-    fn new(a: WhereArchive) -> Self
+    OnMemory(Cursor<Vec<u8>>), FromIO(BufReader<File>)
+}
+impl<'r> From<WhereArchive> for EitherArchiveReader
+{
+    fn from(a: WhereArchive) -> Self
     {
         match a
         {
-            WhereArchive::FromIO(r) => EitherArchiveReader::FromIO(r),
-            WhereArchive::OnMemory(b) => EitherArchiveReader::OnMemory(Cursor::new(b))
-        }
-    }
-    pub fn unwrap(self) -> WhereArchive
-    {
-        match self
-        {
-            EitherArchiveReader::FromIO(r) => WhereArchive::FromIO(r),
-            EitherArchiveReader::OnMemory(c) => WhereArchive::OnMemory(c.into_inner())
+            WhereArchive::FromIO(r) => Self::FromIO(r),
+            WhereArchive::OnMemory(b) => Self::OnMemory(Cursor::new(b))
         }
     }
 }
 impl Read for EitherArchiveReader
 {
-    fn read(&mut self, buf: &mut [u8]) -> IOResult<usize>
+    fn poll_read(self: Pin<&mut Self>, ctx: &mut Context, buf: &mut [u8]) -> Poll<IOResult<usize>>
     {
-        match self
+        match self.get_mut()
         {
-            EitherArchiveReader::FromIO(ref mut r) => r.read(buf),
-            EitherArchiveReader::OnMemory(ref mut c) => c.read(buf)
+            Self::FromIO(ref mut r) => Pin::new(r).poll_read(ctx, buf),
+            Self::OnMemory(ref mut c) => Pin::new(c).poll_read(ctx, buf)
         }
     }
 }
 impl BufRead for EitherArchiveReader
 {
-    fn fill_buf(&mut self) -> IOResult<&[u8]>
+    fn poll_fill_buf(self: Pin<&mut Self>, ctx: &mut Context) -> Poll<IOResult<&[u8]>>
     {
-        match self
+        match self.get_mut()
         {
-            EitherArchiveReader::FromIO(ref mut r) => r.fill_buf(),
-            EitherArchiveReader::OnMemory(ref mut c) => c.fill_buf()
+            Self::FromIO(ref mut r) => Pin::new(r).poll_fill_buf(ctx),
+            Self::OnMemory(ref mut c) => Pin::new(c).poll_fill_buf(ctx)
         }
     }
-    fn consume(&mut self, amt: usize)
+    fn consume(self: Pin<&mut Self>, amt: usize)
     {
-        match self
+        match self.get_mut()
         {
-            EitherArchiveReader::FromIO(ref mut r) => r.consume(amt),
-            EitherArchiveReader::OnMemory(ref mut c) => c.consume(amt)
+            Self::FromIO(ref mut r) => Pin::new(r).consume(amt),
+            Self::OnMemory(ref mut c) => Pin::new(c).consume(amt)
         }
     }
 }
 impl Seek for EitherArchiveReader
 {
-    fn seek(&mut self, pos: SeekFrom) -> IOResult<u64>
+    fn poll_seek(self: Pin<&mut Self>, ctx: &mut Context, pos: SeekFrom) -> Poll<IOResult<u64>>
     {
-        match self
+        match self.get_mut()
         {
-            EitherArchiveReader::FromIO(ref mut r) => r.seek(pos),
-            EitherArchiveReader::OnMemory(ref mut c) => c.seek(pos)
+            Self::FromIO(ref mut r) => Pin::new(r).poll_seek(ctx, pos),
+            Self::OnMemory(ref mut c) => Pin::new(c).poll_seek(ctx, pos)
         }
     }
 }
@@ -219,90 +221,109 @@ pub struct ArchiveRead
 }
 impl ArchiveRead
 {
-    pub fn from_file<P: AsRef<Path>>(path: P, check_integrity: bool) -> ArchiveReadResult<Self>
+    pub async fn from_file<P: AsRef<Path>>(path: P, check_integrity: bool) -> ArchiveReadResult<Self>
     {
-        let mut fi = File::open(path).map(BufReader::new)?;
-        let (comp, crc) = Self::read_file_header(&mut fi)?;
+        let mut fi = File::open(path).await.map(BufReader::new)?;
+        let (comp, crc) = Self::read_file_header(&mut fi).await?;
         let mut body = WhereArchive::FromIO(fi);
         if check_integrity
         {
-            let input_crc = crc32::checksum_ieee(&body.on_memory()?[..]);
+            let input_crc = crc32::checksum_ieee(&body.on_memory().await?[..]);
             if input_crc != crc { return Err(ArchiveReadError::IntegrityCheckFailed); }
         }
 
-        match comp
+        let body = match comp
         {
             CompressionMethod::Lz4(_) =>
             {
-                let mut compressed = Vec::new();
-                EitherArchiveReader::new(body).read_to_end(&mut compressed)?;
-                body = lz4_compression::prelude::decompress(&compressed).map(WhereArchive::OnMemory)?;
+                body.on_memory().await?;
+                let decompressed = async_std::task::spawn_blocking(move ||
+                {
+                    let compressed = body.ref_onmemory_buffer();
+                    lz4_compression::prelude::decompress(&compressed)
+                }).await?;
+
+                WhereArchive::OnMemory(decompressed)
             },
             CompressionMethod::Zlib(ub) =>
             {
-                let mut sink = Vec::with_capacity(ub as _);
-                let reader = EitherArchiveReader::new(body);
-                let mut decoder = zlib::Decoder::new(reader);
-                decoder.read_to_end(&mut sink)?;
-                body = WhereArchive::OnMemory(sink);
+                body.on_memory().await?;
+
+                async_std::task::spawn_blocking(move ||
+                {
+                    let compressed = body.ref_onmemory_buffer();
+                    let mut decoder = zlib::Decoder::new(&compressed[..]);
+                    let mut sink = Vec::with_capacity(ub as _);
+                    std::io::prelude::Read::read_to_end(&mut decoder, &mut sink)?;
+                    
+                    IOResult::Ok(WhereArchive::OnMemory(sink))
+                }).await?
             },
             CompressionMethod::Zstd11(ub) =>
             {
-                let mut sink = Vec::with_capacity(ub as _);
-                let mut decoder = zstd::Decoder::new(EitherArchiveReader::new(body))?;
-                decoder.read_to_end(&mut sink)?;
-                body = WhereArchive::OnMemory(sink);
+                body.on_memory().await?;
+
+                async_std::task::spawn_blocking(move ||
+                {
+                    let compressed = body.ref_onmemory_buffer();
+                    let mut decoder = zstd::Decoder::new(&compressed[..])?;
+                    let mut sink = Vec::with_capacity(ub as _);
+                    std::io::prelude::Read::read_to_end(&mut decoder, &mut sink)?;
+
+                    IOResult::Ok(WhereArchive::OnMemory(sink))
+                }).await?
             },
-            CompressionMethod::None => (/* Nothing to do */)
-        }
-        let mut areader = EitherArchiveReader::new(body);
-        let entries = Self::read_asset_entries(&mut areader)?;
-        let content_baseptr = areader.seek(SeekFrom::Current(0))?;
+            CompressionMethod::None => body
+        };
+        let mut areader = EitherArchiveReader::from(body);
+        let entries = Self::read_asset_entries(&mut areader).await?;
+        let content_baseptr = areader.seek(SeekFrom::Current(0)).await?;
 
         Ok(ArchiveRead { entries, content: areader, content_baseptr })
     }
-    fn read_file_header<R: BufRead>(reader: &mut R) -> ArchiveReadResult<(CompressionMethod, u32)>
+    async fn read_file_header<R: BufRead + Unpin>(reader: &mut R) -> ArchiveReadResult<(CompressionMethod, u32)>
     {
         let mut signature = [0u8; 4];
-        reader.read_exact(&mut signature[..]).map(drop)?;
+        reader.read_exact(&mut signature[..]).await.map(drop)?;
         let mut sink_64_bits = [0u8; 8];
         let comp = match &signature
         {
             b"par " => CompressionMethod::None,
-            b"pard" => reader.read_exact(&mut sink_64_bits)
+            b"pard" => reader.read_exact(&mut sink_64_bits).await
                 .map(|_| CompressionMethod::Zlib(u64::from_le_bytes(sink_64_bits)))?,
-            b"parz" => reader.read_exact(&mut sink_64_bits)
+            b"parz" => reader.read_exact(&mut sink_64_bits).await
                 .map(|_| CompressionMethod::Lz4(u64::from_le_bytes(sink_64_bits)))?,
-            b"par1" => reader.read_exact(&mut sink_64_bits)
+            b"par1" => reader.read_exact(&mut sink_64_bits).await
                 .map(|_| CompressionMethod::Zstd11(u64::from_le_bytes(sink_64_bits)))?,
             _ => return Err(ArchiveReadError::SignatureMismatch)
         };
         let mut crc32_bytes = [0u8; 4];
-        reader.read_exact(&mut crc32_bytes).map(move |_| (comp, u32::from_le_bytes(crc32_bytes))).map_err(From::from)
+        reader.read_exact(&mut crc32_bytes).await
+            .map(move |_| (comp, u32::from_le_bytes(crc32_bytes))).map_err(From::from)
     }
-    fn read_asset_entries<R: BufRead>(reader: &mut R) -> IOResult<HashMap<String, AssetEntryHeadingPair>>
+    async fn read_asset_entries<R: BufRead + Unpin>(reader: &mut R) -> IOResult<HashMap<String, AssetEntryHeadingPair>>
     {
-        let VariableUInt(count) = VariableUInt::read(reader)?;
+        let VariableUInt(count) = VariableUInt::read(reader).await?;
         if count <= 0 { return Ok(HashMap::new()); }
         let mut elements = HashMap::with_capacity(count as _);
         for _ in 0 .. count
         {
-            let heading = AssetEntryHeadingPair::read(reader)?;
-            let PascalString(id_ref) = PascalString::read(reader)?;
+            let heading = AssetEntryHeadingPair::read(reader).await?;
+            let PascalString(id_ref) = PascalString::read(reader).await?;
             elements.insert(id_ref, heading);
         }
         return Ok(elements);
     }
 
-    pub fn read_bin(&mut self, path: &str) -> IOResult<Option<Vec<u8>>>
+    pub async fn read_bin(&mut self, path: &str) -> IOResult<Option<Vec<u8>>>
     {
         if let Some(entry_pair) = self.find(path)
         {
-            self.content.seek(SeekFrom::Start(entry_pair.byte_offset))?;
+            self.content.seek(SeekFrom::Start(entry_pair.byte_offset)).await?;
             let mut sink = Vec::with_capacity(entry_pair.byte_length as _);
             unsafe { sink.set_len(entry_pair.byte_length as _); }
 
-            self.content.read_exact(&mut sink).map(move |_| Some(sink))
+            self.content.read_exact(&mut sink).await.map(move |_| Some(sink))
         }
         else { Ok(None) }
     }
@@ -317,8 +338,6 @@ impl ArchiveRead
             byte_length: x.byte_length, byte_offset: self.content_baseptr + x.relative_offset
         })
     }
-
-    pub fn into_inner_reader(self) -> EitherArchiveReader { self.content }
 }
 #[derive(Debug)]
 pub struct AssetEntryInfo { pub byte_length: u64, pub byte_offset: u64 }
--- serialization-utils/src/lib.rs
+++ serialization-utils/src/lib.rs
@@ -1,49 +1,62 @@
-use std::io::prelude::{BufRead, Write};
+use async_std::io::prelude::{BufRead, Write, ReadExt, WriteExt};
 use std::io::{Result as IOResult, Error as IOError, ErrorKind};
 use std::str::from_utf8;
+use std::pin::Pin;
+
+struct FragmentIterator(u32, bool);
+impl From<u32> for FragmentIterator
+{
+    fn from(v: u32) -> Self { FragmentIterator(v, false) }
+}
+impl Iterator for FragmentIterator
+{
+    type Item = u8;
+    fn next(&mut self) -> Option<u8>
+    {
+        if self.1 { return None; }
+
+        let (n7, nr) = ((self.0 & 0x7f) as u8, self.0 >> 7);
+        if nr == 0 { self.1 = true; }
+        self.0 = nr;
+        
+        Some(n7 | if nr != 0 { 0x80 } else { 0 })
+    }
+}
 
 /// octet variadic unsigned integer
 pub struct VariableUInt(pub u32);
 impl VariableUInt
 {
-    pub fn write<W: Write>(&self, writer: &mut W) -> IOResult<usize>
+    pub async fn write<W: Write + Unpin>(&self, writer: &mut W) -> IOResult<usize>
     {
         let mut iteration_count = 0;
-        Self::iter_fragment(self.0, |v| { iteration_count += 1; writer.write_all(&[v]) }).map(|_| iteration_count)
+        for v in FragmentIterator::from(self.0)
+        {
+            iteration_count += 1;
+            writer.write_all(&[v]).await?;
+        }
+
+        Ok(iteration_count)
     }
-    /// u32 to break apart into bytes, and calls closure with fragment
-    fn iter_fragment<CB: FnMut(u8) -> IOResult<()>>(v: u32, mut callback: CB) -> IOResult<()>
-    {
-        let (n7, nr) = ((v & 0x7f) as u8, v >> 7);
-        callback(n7 | if nr != 0 { 0x80 } else { 0 })?;
-        if nr != 0 { Self::iter_fragment(nr, callback) } else { Ok(()) }
-    }
-    pub fn read<R: BufRead>(reader: &mut R) -> IOResult<Self>
+    pub async fn read<R: BufRead + Unpin>(reader: &mut R) -> IOResult<Self>
     {
         let (mut v, mut shifts) = (0u32, 0usize);
         loop
         {
-            let (consumed, done) =
+            let mut b1 = vec![0u8];
+            match reader.read(&mut b1).await
             {
-                let mut available = match reader.fill_buf()
-                {
-                    Ok(v) => v,
-                    Err(e) => if e.kind() == ErrorKind::Interrupted { continue; } else { return Err(e); }
-                };
-                let (mut consumed, mut done) = (0, false);
-                while !available.is_empty()
-                {
-                    v |= ((available[0] & 0x7f) as u32) << shifts;
-                    shifts += 7;
-                    consumed += 1;
-                    if (available[0] & 0x80) == 0 { done = true; break; }
-                    available = &available[1..];
-                }
-                (consumed, done)
+                Ok(_) => (),
+                Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+                Err(e) => { return Err(e); }
             };
-            reader.consume(consumed);
-            if done { return Ok(VariableUInt(v)); }
+
+            v |= ((b1[0] & 0x7f) as u32) << shifts;
+            shifts += 7;
+            if (b1[0] & 0x80) == 0 { break; }
         }
+
+        Ok(VariableUInt(v))
     }
 }
 
@@ -52,22 +65,27 @@ pub struct PascalString(pub String);
 pub struct PascalStr<'s>(pub &'s str);
 impl PascalString
 {
-    pub fn write<W: Write>(&self, writer: &mut W) -> IOResult<usize> { PascalStr(&self.0).write(writer) }
-
-    pub fn read<R: BufRead>(reader: &mut R) -> IOResult<Self>
+    pub async fn write<W: Write + Unpin>(&self, writer: &mut W) -> IOResult<usize>
     {
-        let VariableUInt(bytelength) = VariableUInt::read(reader)?;
+        PascalStr(&self.0).write(writer).await
+    }
+
+    pub async fn read<R: BufRead + Unpin>(reader: &mut R) -> IOResult<Self>
+    {
+        let VariableUInt(bytelength) = VariableUInt::read(reader).await?;
         let mut bytes = Vec::with_capacity(bytelength as _); unsafe { bytes.set_len(bytelength as _); }
-        reader.read_exact(&mut bytes).map(drop)?;
+        reader.read_exact(&mut bytes).await.map(drop)?;
         
         from_utf8(&bytes[..]).map(|s| PascalString(s.to_owned())).map_err(|e| IOError::new(ErrorKind::Other, e))
     }
 }
 impl<'s> PascalStr<'s>
 {
-    pub fn write<W: Write>(&self, writer: &mut W) -> IOResult<usize>
+    pub async fn write<W: Write + Unpin>(&self, writer: &mut W) -> IOResult<usize>
     {
-        VariableUInt(self.0.as_bytes().len() as _).write(writer)
-            .and_then(|wl| writer.write_all(self.0.as_bytes()).map(move |_| wl + self.0.as_bytes().len()))
+        let wlen = VariableUInt(self.0.as_bytes().len() as _).write(writer).await?;
+        writer.write_all(self.0.as_bytes()).await?;
+
+        Ok(wlen + self.0.as_bytes().len())
     }
 }
