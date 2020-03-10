070cf273b439c6dbe83ace170b0ada17349e5429
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-03-10 09:26:20 +0900

    add: slack notifications

--- .github/actions/integrity-check-slack-notifier/entrypoint.sh
+++ .github/actions/integrity-check-slack-notifier/entrypoint.sh
@@ -1,7 +1,25 @@
 #!/bin/bash -xe
 
-export BUILD_TIME_SECS=0
+# todo: 算出方法を考える
+BUILD_TIME_SECS=0
 
-export COMMIT_INFO="{\"committer\": \"$INPUT_COMMITTER_NAME\", \"message\": \"$INPUT_COMMIT_MESSAGE\"}"
-export PAYLOAD="{\"status\": \"$INPUT_STATUS\", \"build_url\", \"https://github.com/Pctg-x8/peridot/actions/runs/$GITHUB_RUN_ID\", \"number\": \"$GITHUB_RUN_NUMBER\", \"duration\": $BUILD_TIME_SECS, \"compare_url\": \"$INPUT_COMPARE_URL\", \"commit_hash\": \"$GITHUB_SHA\", \"repository\": \"$GITHUB_REPOSITORY\", \"ref\": \"$GITHUB_HEAD_REF\", \"pr_number\": $INPUT_PR_NUMBER, \"commit\": $COMMIT_INFO}"
+# build commit info
+git log --format=%cn\t%B -n 1 $INPUT_HEAD_SHA | read COMMITTER_NAME COMMIT_MESSAGE
+COMMIT_INFO="{\"committer\": \"$COMMITTER_NAME\", \"message\": \"$COMMIT_MESSAGE\", \"sha\": \"$INPUT_HEAD_SHA\"}"
+
+if [ $INPUT_STATUS -eq "failure" ]; then
+    PAYLOAD_STATUS_HEADER="\"status\": \"$INPUT_STATUS\", \"failure_step\": \"$INPUT_FAILURE_STEP\""
+else
+    PAYLOAD_STATUS_HEADER="\"status\": \"$INPUT_STATUS\""
+fi
+
+# urls
+BUILD_URL="https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+COMPARE_URL="https://github.com/$GITHUB_REPOSITORY/compare/$INPUT_BASE_SHA..$INPUT_HEAD_SHA"
+
+# send!
+PAYLOAD="{$PAYLOAD_STATUS_HEADER, \"build_url\": \"$BUILD_URL\", \"number\": \"$GITHUB_RUN_NUMBER\", \"duration\": $BUILD_TIME_SECS, \"compare_url\": \"$COMPARE_URL\", \"commit_hash\": \"$INPUT_HEAD_SHA\", \"repository\": \"$GITHUB_REPOSITORY\", \"ref\": \"$GITHUB_HEAD_REF\", \"pr_number\": $INPUT_PR_NUMBER, \"commit\": $COMMIT_INFO}"
 aws lambda invoke --function-name PeridotIntegrityTestNotificationGHA --invocation-type Event --payload $PAYLOAD out.log
+
+# propagate failure status
+[ $INPUT_STATUS -eq "failure" ] && exit 1
