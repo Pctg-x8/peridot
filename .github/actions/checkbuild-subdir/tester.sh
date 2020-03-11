238db1574a7d14dc36fb2fae9fcfa289611df995
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-03-11 22:34:21 +0900

    fix: auto-detect test or check

--- .github/actions/checkbuild-subdir/tester.sh
+++ .github/actions/checkbuild-subdir/tester.sh
@@ -3,7 +3,12 @@
 cd $INPUT_PATH
 
 export CARGO_TARGET_DIR=$GITHUB_WORKSPACE/.buildcache/target
-export CARGO_HOME=$GITHUB_WORKSPACE/.buildcache 
+export CARGO_HOME=$GITHUB_WORKSPACE/.buildcache
 
-for c in */Cargo.toml
-do (echo testing $c; cd $(dirname $c) && cargo test --no-default-features --verbose); done
+test_or_check() {
+    if `/find-test-code.sh`; then echo "test"; else echo "check"; fi
+}
+
+for c in */Cargo.toml; do
+    (cd $(dirname $c) && cargo `test_or_check` --verbose)
+done
