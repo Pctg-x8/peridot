5443bcaa3635763def75a02a0d95fb80c1108187
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-03-19 21:23:42 +0900

    add: autodelivery script

---
+++ .github/actions/create-dev-delivery-prs/runner.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -xe
+
+PR_URL="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls"
+
+HEAD_NAME=$(echo $GITHUB_REF | sed "s/refs\/heads\///")
+TARGETS=$(git branch -a | sed -n "s/\s*remotes\/origin\/\(dev-.\+\)/\1/p")
+for DEST in TARGETS; do
+    CHANGELOG_LIST=$(git log --oneline --format="- *%h*: %s (by %cn)" remotes/origin/$DEST..$HEAD_NAME)
+    POST_DATA="{\"title\": \"🚚[CHANGELOG-AUTO-DELIVERY]$HEAD_NAME → $DEST🚚\", \"body: \": \"## ChangeLogs\\n\\n$CHANGELOG_LIST\", \"base\": \"$DEST\", \"head\": \"$HEAD_NAME\"}"
+    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $INPUT_TOKEN" -d $POST_DATA $PR_URL
+done
