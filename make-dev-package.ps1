ab9bb251f5792c592f579c05692db35f1dbebee9
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-07-24 21:23:40 +0900

    fix: copy src path to be relative from script path

--- make-dev-package.ps1
+++ make-dev-package.ps1
@@ -3,25 +3,28 @@ param(
     [parameter(HelpMessage = "Package needs zipped?")][switch]$Compress = $false
 )
 
+$ErrorActionPreference = "Stop"
+$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
 New-Item $OutDirectory -ItemType Directory -Force | Out-Null
 Remove-Item $OutDirectory/* -Recurse -Force
 
 # Copy cradles
-robocopy /MIR cradle/windows $OutDirectory/cradle/windows /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock
-robocopy /MIR cradle/mac $OutDirectory/cradle/mac /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock
-robocopy /MIR cradle/linux $OutDirectory/cradle/linux /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock
+robocopy /MIR $ScriptPath/cradle/windows $OutDirectory/cradle/windows /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock
+robocopy /MIR $ScriptPath/cradle/mac $OutDirectory/cradle/mac /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock
+robocopy /MIR $ScriptPath/cradle/linux $OutDirectory/cradle/linux /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock
 
 # Androidはちょっと複雑(無視するやつが多い......)
-robocopy /MIR cradle/android $OutDirectory/cradle/android /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock /xd apkbuild
-robocopy /MIR cradle/android/apkbuild $OutDirectory/cradle/android/apkbuild /xf AndroidManifest.xml /xd res /xf local.properties /xd .gradle /xf build.gradle /xf app.iml /xf libpegamelib.so /xd build /xd .cxx
+robocopy /MIR $ScriptPath/cradle/android $OutDirectory/cradle/android /xd target /xf userlib.rs /xf Cargo.toml /xf Cargo.lock /xd apkbuild
+robocopy /MIR $ScriptPath/cradle/android/apkbuild $OutDirectory/cradle/android/apkbuild /xf AndroidManifest.xml /xd res /xf local.properties /xd .gradle /xf build.gradle /xf app.iml /xf libpegamelib.so /xd build /xd .cxx
 
 # Copy scripts
-Copy-Item build.ps1 $OutDirectory
-Copy-Item build.sh $OutDirectory
+Copy-Item $ScriptPath/build.ps1 $OutDirectory
+Copy-Item $ScriptPath/build.sh $OutDirectory
 
 # Copy tools(for Windows)
 New-Item $OutDirectory/tools -ItemType Directory -Force | Out-Null
-Copy-Item target/release/peridot-*.exe $OutDirectory/tools
+Copy-Item $ScriptPath/target/release/peridot-*.exe $OutDirectory/tools
 
 # Compress(if required)
 if ($Compress) {
