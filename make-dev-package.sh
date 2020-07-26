544a54d6c98c34374f6c6a596efea461b4cdc545
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-07-26 19:05:08 +0900

    fix: rewrite peridot deps and branch specifier

--- make-dev-package.sh
+++ make-dev-package.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
+shopt -s globstar
 
 usage_exit() {
     echo "usage>$0 [-o OutDirectory] [-c]"
@@ -9,11 +10,15 @@ usage_exit() {
 
 SCRIPT_PATH=$(dirname $0)
 OUT_DIRECTORY="peridot-sdk"
+PERIDOT_BRANCH="dev"
 COMPRESS=0
-while getopts o:ch OPT; do
+while getopts ob:ch OPT; do
     case $OPT in
         o)
             OUT_DIRECTORY=$OPTARG
+            ;;
+        b)
+            PERIDOT_BRANCH=$OPTARG
             ;;
         c)
             COMPRESS=1
@@ -34,7 +39,7 @@ rm -rf $OUT_DIRECTORY/*
 mkdir -p $OUT_DIRECTORY/cradle/windows
 rsync -auz --progress $SCRIPT_PATH/cradle/windows $OUT_DIRECTORY/cradle/ --exclude target --exclude userlib.rs --exclude Cargo.toml --exclude Cargo.lock
 mkdir -p $OUT_DIRECTORY/cradle/mac
-rsync -auz --progress $SCRIPT_PATH/cradle/mac $OUT_DIRECTORY/cradle/ --exclude target --exclude userlib.rs --exclude Cargo.toml --exclude Cargo.lock --exclude build --exclude assets.par --exclude xcuserdata
+rsync -auz --progress $SCRIPT_PATH/cradle/mac $OUT_DIRECTORY/cradle/ --exclude target --exclude userlib.rs --exclude Cargo.toml --exclude Cargo.lock --exclude build --exclude assets.par --exclude xcuserdata --exclude rlibs
 mkdir -p $OUT_DIRECTORY/cradle/linux
 rsync -auz --progress $SCRIPT_PATH/cradle/linux $OUT_DIRECTORY/cradle/ --exclude target --exclude userlib.rs --exclude Cargo.toml --exclude Cargo.lock
 
@@ -42,6 +47,13 @@ rsync -auz --progress $SCRIPT_PATH/cradle/linux $OUT_DIRECTORY/cradle/ --exclude
 mkdir -p $OUT_DIRECTORY/cradle/android
 rsync -auz --progress $SCRIPT_PATH/cradle/android $OUT_DIRECTORY/cradle --exclude target --exclude userlib.rs --exclude Cargo.toml --exclude Cargo.lock --exclude apkbuild
 rsync -auz --progress $SCRIPT_PATH/cradle/android/apkbuild $OUT_DIRECTORY/cradle/android --exclude AndroidManifest.xml --exclude res --exclude local.properties --exclude .gradle --exclude build.gradle --exclude libpegamelib.so --exclude build --exclude .cxx
+
+# Rewrite manifest peridot path
+for f in $OUT_DIRECTORY/cradle/**/Cargo.template.toml; do
+    echo "rewriting peridot deps in $f..."
+    sed -i.o -e "s/peridot = { path = \"..\\/..\" }/peridot = { git = \"https:\\/\\/github.com\\/Pctg-x8\\/peridot\", branch = \"$PERIDOT_BRANCH\" }/g" $f
+    rm $f.o
+done
 
 # Copy scripts
 cp $SCRIPT_PATH/build.ps1 $OUT_DIRECTORY
