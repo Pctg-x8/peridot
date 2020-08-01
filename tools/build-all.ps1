2e7f3df560c58874c44729123de7c288989e8817
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-08-01 16:51:11 +0900

    fix: redirect specifier in script

--- tools/build-all.ps1
+++ tools/build-all.ps1
@@ -7,7 +7,8 @@ try {
     $Targets = Get-ChildItem $ScriptPath -Directory -Name | Where-Object { Test-Path $ScriptPath/$_/Cargo.toml }
     foreach ($path in $Targets) {
         Write-Host "Building: $path"
-        Set-Location $ScriptPath/$path; cargo build --release
+        Set-Location $ScriptPath/$path
+        cargo build --release 2>&1 | %{ "$_" }
     }
 }
 finally { Pop-Location }
