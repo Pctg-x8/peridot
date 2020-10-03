e0e8b7f63712221da70aa4e056700f9d4df4533f
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-10-03 10:00:58 +0900

    add: rendering pass with depth

--- src/lib.rs
+++ src/lib.rs
@@ -489,7 +489,27 @@ impl RenderPassTemplates
         b.add_subpass(br::SubpassDescription::new().add_color_output(0, br::ImageLayout::ColorAttachmentOpt, None));
         b.add_dependency(SubpassDependencyTemplates::to_color_attachment_in(None, 0, true));
 
-        return b;
+        b
+    }
+    pub fn single_render_with_depth_noread(
+        format: br::vk::VkFormat, depth_format: br::vk::VkFormat
+    ) -> br::RenderPassBuilder {
+        let mut b = br::RenderPassBuilder::new();
+        let adesc = br::AttachmentDescription::new(format, br::ImageLayout::PresentSrc, br::ImageLayout::PresentSrc)
+            .load_op(br::LoadOp::Clear).store_op(br::StoreOp::Store);
+        let addesc = br::AttachmentDescription::new(
+            depth_format, br::ImageLayout::DepthStencilAttachmentOpt, br::ImageLayout::DepthStencilAttachmentOpt
+        )
+        .load_op(br::LoadOp::Clear).store_op(br::StoreOp::DontCare);
+        b.add_attachments(vec![adesc, addesc]);
+        b.add_subpass(
+            br::SubpassDescription::new()
+                .add_color_output(0, br::ImageLayout::ColorAttachmentOpt, None)
+                .depth_stencil(1, br::ImageLayout::DepthStencilAttachmentOpt)
+        );
+        b.add_dependency(SubpassDependencyTemplates::to_color_attachment_in(None, 0, true));
+
+        b
     }
 }
 
