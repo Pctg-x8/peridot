3f0eda6f865ff671fa61e9e0b78dcce6b40e8b64
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-11-07 19:28:08 +0900

    fix: feature gating

--- cradle/windows/build.rs
+++ cradle/windows/build.rs
@@ -1,4 +1,6 @@
 fn main() {
     #[cfg(not(feature = "bedrock/DynamicLoaded"))]
-    println!("cargo:rustc-link-search=static={}/Lib", env!("VK_SDK_PATH"));
+    {
+        println!("cargo:rustc-link-search=static={}/Lib", env!("VK_SDK_PATH"));
+    }
 }
