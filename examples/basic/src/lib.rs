f27bd42ba49eba81b1c50449c602b153d5a29d9a
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-11-07 22:34:38 +0900

    fix: missing type variable

--- examples/basic/src/lib.rs
+++ examples/basic/src/lib.rs
@@ -2,7 +2,7 @@
 use std::marker::PhantomData;
 
 pub struct Game<NL>(PhantomData<*const NL>);
-impl<PL: peridot::NativeLinker> Game<NL>
+impl<NL: peridot::NativeLinker> Game<NL>
 {
     pub const NAME: &'static str = "Peridot Examples - Basic";
     pub const VERSION: (u32, u32, u32) = (0, 1, 0);
@@ -10,5 +10,5 @@ impl<PL: peridot::NativeLinker> Game<NL>
 impl<NL: peridot::NativeLinker> peridot::FeatureRequests for Game<NL> {}
 impl<NL: peridot::NativeLinker> peridot::EngineEvents<NL> for Game<NL>
 {
-    fn init(_e: &mut peridot::Engine<NL>) -> Self { Empty(PhantomData) }
+    fn init(_e: &mut peridot::Engine<NL>) -> Self { Game(PhantomData) }
 }
