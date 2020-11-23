1ea09ad6523f1ffd11f1a309524e75ce64a7781a
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-11-24 08:51:34 +0900

    add: aspect ratio in projection matrix calcs

--- math/src/gaming.rs
+++ math/src/gaming.rs
@@ -22,7 +22,7 @@ pub enum ProjectionMethod {
 ///     projection: ProjectionMethod::Orthographic { size: 5.0 }, position: Vector3::ZERO, rotation: Quaternion::ONE,
 ///     depth_range: 1.0 .. 9.0
 /// };
-/// let (mv, mp) = c.matrixes();
+/// let (mv, mp) = c.matrixes(1.0);
 /// assert_eq!(mv.clone() * Vector3(5.0, 0.0, 1.0), Vector4(5.0, 0.0, 1.0, 1.0));
 /// assert_eq!(mp * mv * Vector3(5.0, 0.0, 1.0), Vector4(1.0, 0.0, 0.0, 1.0));
 /// ```
@@ -33,21 +33,23 @@ pub struct Camera {
 
 impl Camera {
     /// calculates the camera projection matrix
-    pub fn projection_matrix(&self) -> Matrix4F32 {
+    pub fn projection_matrix(&self, aspect_wh: f32) -> Matrix4F32 {
         match self.projection {
             ProjectionMethod::Perspective { fov } => {
-                let scaling = (fov / 2.0).tan().recip();
+                let scaling_tan = (fov / 2.0).tan();
                 let zdiff = self.depth_range.end - self.depth_range.start;
                 let zscale = (self.depth_range.end / zdiff,
                     -(self.depth_range.end * self.depth_range.start) / zdiff);
                 
-                Matrix4([scaling, 0.0, 0.0, 0.0], [0.0, scaling, 0.0, 0.0],
-                    [0.0, 0.0, zscale.0, zscale.1], [0.0, 0.0, 1.0, 0.0])
+                Matrix4(
+                    [(aspect_wh * scaling_tan).recip(), 0.0, 0.0, 0.0], [0.0, scaling_tan.recip(), 0.0, 0.0],
+                    [0.0, 0.0, zscale.0, zscale.1], [0.0, 0.0, 1.0, 0.0]
+                )
             },
             ProjectionMethod::Orthographic { size } => {
                 let zdiff = self.depth_range.end - self.depth_range.start;
                 let t = Matrix4::translation(Vector3(0.0, 0.0, -self.depth_range.start));
-                let s = Matrix4::scale(Vector4(size.recip(), size.recip(), zdiff.recip(), 1.0));
+                let s = Matrix4::scale(Vector4((aspect_wh * size).recip(), size.recip(), zdiff.recip(), 1.0));
 
                 s * t
             },
@@ -65,8 +67,8 @@ impl Camera {
         Matrix4F32::from(-self.rotation.clone()) * Matrix4F32::translation(-self.position.clone())
     }
     /// calculates the camera view matrix and the projection matrix(returns in this order)
-    pub fn matrixes(&self) -> (Matrix4F32, Matrix4F32) {
-        (self.view_matrix(), self.projection_matrix())
+    pub fn matrixes(&self, aspect_wh: f32) -> (Matrix4F32, Matrix4F32) {
+        (self.view_matrix(), self.projection_matrix(aspect_wh))
     }
 
     /// Sets rotation of the camera to look at a point
