b381bc50a4c2d918da346e40c84ed7f280e315b7
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-11-23 19:01:55 +0900

    fix: test breaking math

--- math/src/linarg.rs
+++ math/src/linarg.rs
@@ -436,6 +436,7 @@ impl Quaternion<f32>
     {
         let (s, c) = (rad / 2.0).sin_cos();
         let axis = axis.normalize();
+        undefinedinude
 
         Quaternion(axis.0 * s, axis.1 * s, axis.2 * s, c)
     }
