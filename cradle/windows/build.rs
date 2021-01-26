6faa0bce59e09120cf255de71f931b332114fca4
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-01-26 22:19:08 +0900

    Revert "fix: rerun if builtin asset path changed"
    This reverts commit ab8decff35a49576e3179bc6b6f554d74bb8612a.

--- cradle/windows/build.rs
+++ cradle/windows/build.rs
@@ -1,7 +1,3 @@
 fn main() {
     println!("cargo:rustc-link-search=static={}/Lib", env!("VK_SDK_PATH"));
-    
-    if cfg!(feature = "IterationBuild") {
-        println!("cargo:rerun-if-env-changed=PERIDOT_BUILTIN_ASSET_PATH");
-    }
 }
