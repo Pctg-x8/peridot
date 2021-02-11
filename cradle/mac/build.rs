a56a650fc39bbaf26926101f09dfbf1a8e3bbd00
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-02-11 17:29:54 +0900

    fix: moltenvk sdk base path from official env variable

--- cradle/mac/build.rs
+++ cradle/mac/build.rs
@@ -1,7 +1,9 @@
 
 fn main() {
-    println!("cargo:rustc-link-search=framework={}/MoltenVK/macOS", env!("VK_SDK_PATH"));
-    println!("cargo:rustc-link-search=framework={}/macOS/Frameworks", env!("VK_SDK_PATH"));
+    let mvk_sdk_path = std::path::PathBuf::from(env!("VULKAN_SDK")).parent().expect("parent detection failed").to_path_buf();
+
+    println!("cargo:rustc-link-search=framework={}", mvk_sdk_path.join("MoltenVK").display());
+    println!("cargo:rustc-link-search=framework={}", mvk_sdk_path.join("macOS/Frameworks").display());
     println!("cargo:rustc-link-lib=c++");
     println!("cargo:rustc-link-lib=framework=IOSurface");
     println!("cargo:rustc-link-lib=framework=IOKit");
