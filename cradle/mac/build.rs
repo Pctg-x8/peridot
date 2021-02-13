7fb99083436bbe9c1b1b099fc8a57295ecc96a6c
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-02-13 19:40:48 +0900

    fix: format

--- cradle/mac/build.rs
+++ cradle/mac/build.rs
@@ -1,8 +1,13 @@
-
 fn main() {
-    let vk_sdk_base = std::path::PathBuf::from(env!("VULKAN_SDK")).parent().expect("Failed to calc parent for VULKAN_SDK").to_path_buf();
+    let vk_sdk_base = std::path::PathBuf::from(env!("VULKAN_SDK"))
+        .parent()
+        .expect("Failed to calc parent for VULKAN_SDK")
+        .to_path_buf();
 
-    println!("cargo:rustc-link-search=framework={}", vk_sdk_base.join("MoltenVK/macOS").display());
+    println!(
+        "cargo:rustc-link-search=framework={}",
+        vk_sdk_base.join("MoltenVK/macOS").display()
+    );
     println!("cargo:rustc-link-lib=c++");
     println!("cargo:rustc-link-lib=framework=IOSurface");
     println!("cargo:rustc-link-lib=framework=IOKit");
