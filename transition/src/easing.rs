2b8af508678f0ad55e1d7cedc6326aff669537d4
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-02-27 17:56:07 +0900

    rm: const

--- transition/src/easing.rs
+++ transition/src/easing.rs
@@ -3,15 +3,15 @@
 pub const fn linear(x: f32) -> f32 {
     x
 }
-pub const fn quadratic(x: f32) -> f32 {
+pub fn quadratic(x: f32) -> f32 {
     x.powi(2)
 }
-pub const fn cubic(x: f32) -> f32 {
+pub fn cubic(x: f32) -> f32 {
     x.powi(3)
 }
-pub const fn quartic(x: f32) -> f32 {
+pub fn quartic(x: f32) -> f32 {
     x.powi(4)
 }
-pub const fn quintic(x: f32) -> f32 {
+pub fn quintic(x: f32) -> f32 {
     x.powi(5)
 }
