7ccd621d4ca61311d3b3d8e7199e3d8c6d36b9ad
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-03-29 10:17:34 +0900

    rm: inline config

--- examples/vg/src/lib.rs
+++ examples/vg/src/lib.rs
@@ -46,7 +46,7 @@ pub struct Game<PL: peridot::NativeLinker> {
     ph: PhantomData<*const PL>,
 }
 impl<PL: peridot::NativeLinker> Game<PL> {
-    pub const NAME: &'static str = "Peridot Examples - VectorGraphics";
+    pub const NAME: &'static str = "";
     pub const VERSION: (u32, u32, u32) = (0, 1, 0);
 }
 impl<PL: peridot::NativeLinker> peridot::FeatureRequests for Game<PL> {}
