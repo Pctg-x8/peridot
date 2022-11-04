136a537b79fc85e10dc557c5c163e0de1314d615
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-11-05 01:23:40 +0900

    fix: vg font entry points

--- vg/src/font/dwrite_driver.rs
+++ vg/src/font/dwrite_driver.rs
@@ -1,8 +1,8 @@
 use euclid::point2;
 use log::*;
 use lyon_path::PathEvent;
-use std::ptr::null_mut;
 use std::slice::from_raw_parts;
+use std::{cell::RefCell, ptr::null_mut};
 use windows::Win32::Graphics::{
     Direct2D::Common::{
         ID2D1SimplifiedGeometrySink, ID2D1SimplifiedGeometrySink_Impl, D2D1_FIGURE_END_CLOSED,
@@ -16,21 +16,25 @@ use windows::Win32::Graphics::{
 
 #[windows::core::implement(ID2D1SimplifiedGeometrySink)]
 pub struct PathEventReceiver {
-    paths: Vec<PathEvent>,
+    paths: RefCell<Vec<PathEvent>>,
 }
 impl PathEventReceiver {
     pub fn new() -> Self {
-        PathEventReceiver { paths: Vec::new() }
+        PathEventReceiver {
+            paths: RefCell::new(Vec::new()),
+        }
     }
 
     pub fn drain_all_paths(&mut self) -> std::vec::Drain<PathEvent> {
-        self.paths.drain(..)
+        self.paths.get_mut().drain(..)
     }
 }
 impl ID2D1SimplifiedGeometrySink_Impl for PathEventReceiver {
     fn AddLines(&self, p: *const D2D_POINT_2F, count: u32) {
         for p in unsafe { from_raw_parts(p, count as _) } {
-            self.paths.push(PathEvent::LineTo(point2(p.x, -p.y)));
+            self.paths
+                .borrow_mut()
+                .push(PathEvent::LineTo(point2(p.x, -p.y)));
         }
     }
 
@@ -45,22 +49,22 @@ impl ID2D1SimplifiedGeometrySink_Impl for PathEventReceiver {
                 point2(p.point2.x, -p.point2.y),
             );
             let p3 = point2(p.point3.x, -p.point3.y);
-            self.paths.push(PathEvent::CubicTo(p1, p2, p3));
+            self.paths.borrow_mut().push(PathEvent::CubicTo(p1, p2, p3));
         }
     }
 
     fn BeginFigure(
         &self,
         startpoint: &D2D_POINT_2F,
-        figurebegin: windows::Win32::Graphics::Direct2D::Common::D2D1_FIGURE_BEGIN,
+        _figurebegin: windows::Win32::Graphics::Direct2D::Common::D2D1_FIGURE_BEGIN,
     ) {
         let p = point2(startpoint.x, -startpoint.y);
-        self.paths.push(PathEvent::MoveTo(p));
+        self.paths.borrow_mut().push(PathEvent::MoveTo(p));
     }
 
     fn EndFigure(&self, figureend: windows::Win32::Graphics::Direct2D::Common::D2D1_FIGURE_END) {
         if figureend == D2D1_FIGURE_END_CLOSED {
-            self.paths.push(PathEvent::Close);
+            self.paths.borrow_mut().push(PathEvent::Close);
         }
     }
 
@@ -69,14 +73,19 @@ impl ID2D1SimplifiedGeometrySink_Impl for PathEventReceiver {
     }
 
     fn SetFillMode(&self, fillmode: windows::Win32::Graphics::Direct2D::Common::D2D1_FILL_MODE) {
-        trace!("*UNIMPLEMENTED* SetFillMode with {fillmode}");
+        trace!("*UNIMPLEMENTED* SetFillMode with {fillmode:?}");
     }
 
     fn SetSegmentFlags(
         &self,
         vertexflags: windows::Win32::Graphics::Direct2D::Common::D2D1_PATH_SEGMENT,
     ) {
-        trace!("*UNIMPLEMENTED* SetSegmentFlags with {vertexflags}");
+        trace!("*UNIMPLEMENTED* SetSegmentFlags with {vertexflags:?}");
+    }
+}
+impl From<&'_ PathEventReceiver> for &'_ ID2D1SimplifiedGeometrySink {
+    fn from(value: &'_ PathEventReceiver) -> Self {
+        unsafe { std::mem::transmute(value) }
     }
 }
 
@@ -85,7 +94,7 @@ impl<'a> ATFRegisterScope<'a> {
     pub fn register(
         factory: &'a IDWriteFactory,
         atf: AssetToFontConverter,
-    ) -> std::io::Result<Self> {
+    ) -> windows::core::Result<Self> {
         unsafe {
             factory
                 .RegisterFontFileLoader(&atf)
@@ -109,23 +118,35 @@ impl Drop for ATFRegisterScope<'_> {
 
 #[windows::core::implement(IDWriteFontFileLoader)]
 pub struct AssetToFontConverter {
-    asset: Option<super::TTFBlob>,
+    asset: RefCell<Option<super::TTFBlob>>,
 }
 impl AssetToFontConverter {
     pub fn new(asset: super::TTFBlob) -> Self {
-        Self { asset: Some(asset) }
+        Self {
+            asset: RefCell::new(Some(asset)),
+        }
     }
 }
 impl IDWriteFontFileLoader_Impl for AssetToFontConverter {
     fn CreateStreamFromKey(
         &self,
-        fontfilereferencekey: *const core::ffi::c_void,
-        fontfilereferencekeysize: u32,
+        _fontfilereferencekey: *const core::ffi::c_void,
+        _fontfilereferencekeysize: u32,
     ) -> windows::core::Result<windows::Win32::Graphics::DirectWrite::IDWriteFontFileStream> {
         unsafe {
-            AssetStreamBridge::new(self.asset.take().expect("ATF create stream called twice?"))
-                .cast()
+            AssetStreamBridge::new(
+                self.asset
+                    .borrow_mut()
+                    .take()
+                    .expect("ATF create stream called twice?"),
+            )
+            .cast()
         }
+    }
+}
+impl From<&'_ AssetToFontConverter> for &'_ IDWriteFontFileLoader {
+    fn from(value: &'_ AssetToFontConverter) -> Self {
+        unsafe { std::mem::transmute(value) }
     }
 }
 
@@ -151,7 +172,7 @@ impl IDWriteFontFileStream_Impl for AssetStreamBridge {
         &self,
         fragmentstart: *mut *mut core::ffi::c_void,
         fileoffset: u64,
-        fragmentsize: u64,
+        _fragmentsize: u64,
         fragmentcontext: *mut *mut core::ffi::c_void,
     ) -> windows::core::Result<()> {
         unsafe {
@@ -162,5 +183,5 @@ impl IDWriteFontFileStream_Impl for AssetStreamBridge {
         Ok(())
     }
 
-    fn ReleaseFileFragment(&self, fragmentcontext: *mut core::ffi::c_void) {}
+    fn ReleaseFileFragment(&self, _fragmentcontext: *mut core::ffi::c_void) {}
 }
