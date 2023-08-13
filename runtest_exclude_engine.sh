cd104bd7c7df037db299762436b8e87c56857d93
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-08-14 00:48:15 +0900

    fix: move modules

--- runtest_exclude_engine.sh
+++ runtest_exclude_engine.sh
@@ -11,8 +11,8 @@ for c in !(extras|cradle|examples)/!(shaderbuild)/Cargo.toml
 do (echo testing $c; cd $(dirname $c) && cargo test --verbose) || exit 2; done
 
 # for peridot-dependent modules
-(echo checking vertex-processing-pack; cd vertex-processing-pack && cargo check --verbose --features=bedrock/VK_EXT_debug_report) || exit 3
-(echo checking vg; cd vg && cargo check --verbose --features=bedrock/VK_EXT_debug_report) || exit 3
+(echo checking vertex-processing-pack; cd modules/vertex-processing-pack && cargo check --verbose --features=bedrock/VK_EXT_debug_report) || exit 3
+(echo checking vg; cd modules/vg && cargo check --verbose --features=bedrock/VK_EXT_debug_report) || exit 3
 # for peridot-dependent tools
 (echo checking tools/shaderbuild; cd tools/shaderbuild && cargo check --verbose --features=bedrock/VK_EXT_debug_report) || exit 4
 
