6095034281569e159d0fe885458c4bc90005f807
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-08-27 12:21:22 +0900

    fix: add authenticating google cloud in doc deployment

--- .github/workflows/generator/app/Main.hs
+++ .github/workflows/generator/app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Data.Aeson.Yaml (encode)
 import Data.ByteString.Lazy.Char8 qualified as LBS8
+import DocumentDeployment qualified
 import IntegrityTest.PullRequestTriggered
 import IntegrityTest.Weekly
 import System.Environment (getArgs)
@@ -12,3 +13,4 @@ main = do
   dest <- head <$> getArgs
   LBS8.writeFile (dest </> "integrity-test.yml") $ encode integrityTest
   LBS8.writeFile (dest </> "weekly-integrity-test.yml") $ encode weeklyIntegrityTest
+  LBS8.writeFile (dest </> "docs-cd.yml") $ encode DocumentDeployment.workflow
--- .github/workflows/generator/src/Workflow/GitHub/Actions.hs
+++ .github/workflows/generator/src/Workflow/GitHub/Actions.hs
@@ -24,9 +24,11 @@ module Workflow.GitHub.Actions
     Workflow (..),
     WorkflowTrigger (..),
     WorkflowPullRequestTrigger (..),
+    WorkflowPushTrigger (..),
     WorkflowScheduleTrigger (..),
     WorkflowScheduleTimer (..),
     onPullRequest,
+    onPush,
     scheduled,
     PermissionTable (..),
     PermissionKey (..),
@@ -207,25 +209,40 @@ jobForwardingStepOutput stepName key = jobOutput key $ mkRefStepOutputExpression
 jobRunsOn :: [String] -> Job -> Job
 jobRunsOn platform self = self {runsOn = platform}
 
-data WorkflowTrigger = OnEvent String | OnEvents [String] | OnEventsDetailed (Maybe WorkflowPullRequestTrigger) (Maybe WorkflowPullRequestTrigger) (Maybe WorkflowPushTrigger) (Maybe WorkflowScheduleTrigger)
+data WorkflowTrigger = WorkflowTrigger
+  { workflowTriggerOnPullRequest :: Maybe WorkflowPullRequestTrigger,
+    workflowTriggerOnPullRequestTarget :: Maybe WorkflowPullRequestTrigger,
+    workflowTriggerOnPush :: Maybe WorkflowPushTrigger,
+    workflowTriggerOnSchedule :: Maybe WorkflowScheduleTrigger
+  }
 
 instance ToJSON WorkflowTrigger where
-  toJSON (OnEvent event) = toJSON event
-  toJSON (OnEvents events) = toJSON events
-  toJSON (OnEventsDetailed onPullRequest' onPullRequestTarget onPush onSchedule) =
+  toJSON t =
     object $
       catMaybes
-        [ ("pull_request" .=) <$> onPullRequest',
-          ("pull_request_target" .=) <$> onPullRequestTarget,
-          ("push" .=) <$> onPush,
-          ("schedule" .=) <$> onSchedule
+        [ ("pull_request" .=) <$> workflowTriggerOnPullRequest t,
+          ("pull_request_target" .=) <$> workflowTriggerOnPullRequestTarget t,
+          ("push" .=) <$> workflowTriggerOnPush t,
+          ("schedule" .=) <$> workflowTriggerOnSchedule t
         ]
 
+_emptyWorkflowTrigger :: WorkflowTrigger
+_emptyWorkflowTrigger =
+  WorkflowTrigger
+    { workflowTriggerOnPullRequest = Nothing,
+      workflowTriggerOnPullRequestTarget = Nothing,
+      workflowTriggerOnPush = Nothing,
+      workflowTriggerOnSchedule = Nothing
+    }
+
 onPullRequest :: WorkflowPullRequestTrigger -> WorkflowTrigger
-onPullRequest details = OnEventsDetailed (Just details) Nothing Nothing Nothing
+onPullRequest details = _emptyWorkflowTrigger {workflowTriggerOnPullRequest = Just details}
+
+onPush :: WorkflowPushTrigger -> WorkflowTrigger
+onPush details = _emptyWorkflowTrigger {workflowTriggerOnPush = Just details}
 
 scheduled :: [WorkflowScheduleTimer] -> WorkflowTrigger
-scheduled timers = OnEventsDetailed Nothing Nothing Nothing $ Just $ WorkflowScheduleTrigger timers
+scheduled timers = _emptyWorkflowTrigger {workflowTriggerOnSchedule = Just $ WorkflowScheduleTrigger timers}
 
 data WorkflowPullRequestTrigger = WorkflowPullRequestTrigger
   { prTriggerBranches :: [String],
