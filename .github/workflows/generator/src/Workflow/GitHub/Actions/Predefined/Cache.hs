f92a6575d07551f68bc8f2c963f7ba299b6e6e59
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2024-11-05 10:20:10 +0900

    fix: upgrade cache v4

--- .github/workflows/generator/src/Workflow/GitHub/Actions/Predefined/Cache.hs
+++ .github/workflows/generator/src/Workflow/GitHub/Actions/Predefined/Cache.hs
@@ -7,7 +7,7 @@ import Workflow.GitHub.Actions qualified as GHA
 import Workflow.GitHub.Actions.ExpressionBuilder (mkRefStepOutputExpression)
 
 step :: [String] -> String -> GHA.Step
-step paths key = GHA.actionStep "actions/cache@v3" params
+step paths key = GHA.actionStep "actions/cache@v4" params
   where
     params = M.fromList [("path", toJSON $ intercalate "\n" paths), ("key", toJSON key)]
 
