17a92feb6f8633c8a8005f9334d17f77c4215a9e
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2024-11-07 16:38:48 +0900

    fix: sync from_file errtype conversion

--- modules/vertex-processing-pack/src/lib.rs
+++ modules/vertex-processing-pack/src/lib.rs
@@ -447,7 +447,7 @@ impl<R: BufRead + Seek> PvpContainerReader<R> {
 }
 impl PvpContainerReader<BufReader<File>> {
     pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, PvpContainerReadError> {
-        File::open(path).and_then(|fp| Self::new(BufReader::new(fp)))
+        Ok(Self::new(BufReader::new(File::open(path)?)))
     }
 }
 
