c32936017bb97ed60eb5bbbef2bb3517b21c890b
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2024-12-24 00:17:49 +0900

    add: linefeed checker

---
+++ .github/actions/codeform-checker/linefeed_checker.py
@@ -0,0 +1,17 @@
+from testutil import target_sources, annotate_error
+
+incorrect_line_feeds = []
+for fpath in target_sources():
+    with open(fpath, "rb") as fp:
+        buf = bytearray(64 * 1024)
+        while fp.readinto1(buf):
+            if b'r' in buf:
+                incorrect_line_feeds.append(fpath)
+                break
+
+if not incorrect_line_feeds:
+    print("All files are terminated with LF")
+else:
+    for fpath in incorrect_line_feeds:
+        annotate_error(fpath, 1, 1, f"This file contains CR character")
+    exit(1)
