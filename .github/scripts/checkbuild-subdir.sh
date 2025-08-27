53ecbde3b143c32a51edbfada5e7ac3c755cdfd5
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2025-08-27 09:11:41 +0900

    fix: decompose run_test

--- .github/scripts/checkbuild-subdir.sh
+++ .github/scripts/checkbuild-subdir.sh
@@ -16,19 +16,17 @@ test_or_check() {
     echo "check"
 }
 
-run_test() {
+for c in **/*/Cargo.toml
+do
+    pushd $(dirname $c)
+    
     if [ -e ./ci-test.sh ]
     then
         ./ci-test.sh
     else
         cargo `test_or_check` --verbose --message-format=json | $HOME/.local/bin/cargo-json-gha-translator
     fi
-}
 
-for c in **/*/Cargo.toml
-do
-    pushd $(dirname $c)
-    run_test
     popd
 done
 
