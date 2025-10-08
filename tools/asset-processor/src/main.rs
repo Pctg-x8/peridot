3e09f7e843de4743b757b7ccfbc0df3e54587c2c
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2025-10-08 10:15:26 +0900

    add: asset processor abstraction

--- tools/asset-processor/src/main.rs
+++ tools/asset-processor/src/main.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use std::{
+    ffi::OsStr,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
 use ktx::Texture;
@@ -12,90 +15,94 @@ pub struct Args {
     out_dir: Option<PathBuf>,
 }
 
-fn main() {
-    tracing_subscriber::fmt()
-        .pretty()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .init();
+pub trait AssetProcessor {
+    fn can_process(&self, source_path: &Path) -> bool;
+    fn dest_path(&self, source_file_name: &OsStr, out_dir_path: &Path) -> PathBuf;
+    fn process(
+        &self,
+        source_path: &Path,
+        out_path: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>>;
+}
 
-    let args = Args::parse();
-    let span = tracing::span!(
-        tracing::Level::INFO,
-        "main",
-        source_path = %args.source_path.display(),
-    );
-    let _span_enter = span.enter();
+#[derive(thiserror::Error, Debug)]
+pub enum RenderingConfigurationAssetProcessError {
+    #[error("Failed to spawn compiler process: {0}")]
+    SpawnCompilerProcessFailed(std::io::Error),
+    #[error("Failed to wait process: {0}")]
+    ProcessWaitFailed(std::io::Error),
+}
 
-    let ext = args.source_path.extension();
-    if ext.is_some_and(|x| x == "prc") {
-        // rendering configuration: just compile
-        let dest_path = args
-            .out_dir
-            .as_deref()
-            .unwrap_or_else(|| args.source_path.parent().expect("no parent?"))
-            .join(
-                args.source_path
-                    .file_name()
-                    .expect("no file name in source path"),
-            )
-            .with_extension("pa1-rendering-configuration");
-        if !args.force_rebuild
-            && let (Ok(x), Ok(y)) = (args.source_path.metadata(), dest_path.metadata())
-            && x.modified().unwrap() <= y.modified().unwrap()
-        {
-            tracing::info!(reason = "modified time", "skip asset");
-            return;
-        }
+pub struct RenderingConfigurationAssetProcessor;
+impl AssetProcessor for RenderingConfigurationAssetProcessor {
+    fn can_process(&self, source_path: &Path) -> bool {
+        source_path.extension().is_some_and(|x| x == "prc")
+    }
 
-        let mut process = match std::process::Command::new(
+    fn dest_path(&self, source_file_name: &OsStr, out_dir_path: &Path) -> PathBuf {
+        out_dir_path
+            .join(source_file_name)
+            .with_extension("pa1-rendering-configuration")
+    }
+
+    fn process(
+        &self,
+        source_path: &Path,
+        out_path: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        std::process::Command::new(
             std::env::current_exe()
                 .expect("current_exe")
                 .parent()
                 .expect("dirname")
                 .join("peridot-rendering-configuration-compiler"),
         )
-        .arg(&args.source_path)
+        .arg(source_path)
         .arg("--output")
-        .arg(&dest_path)
+        .arg(out_path)
         .spawn()
-        {
-            Ok(x) => x,
-            Err(e) => {
-                tracing::error!(reason = ?e, "Failed to spawn peridot-rendering-configuration-compiler");
-                std::process::exit(1);
-            }
-        };
-        if let Err(e) = process.wait() {
-            tracing::error!(reason = ?e, "process.wait failed");
-            std::process::exit(1);
-        }
-    } else if ext.is_some_and(|x| x == "png" || x == "jpg" || x == "tiff") {
-        // image asset: decompress to rgba and recompress(TODO: if needed, specified by metadata file)
-        let dest_path = args
-            .out_dir
-            .as_deref()
-            .unwrap_or_else(|| args.source_path.parent().expect("no parent?"))
-            .join(
-                args.source_path
-                    .file_name()
-                    .expect("no file name in source path"),
-            )
-            .with_extension("pa1-texture2d");
-        if !args.force_rebuild
-            && let (Ok(x), Ok(y)) = (args.source_path.metadata(), dest_path.metadata())
-            && x.modified().unwrap() <= y.modified().unwrap()
-        {
-            tracing::info!(reason = "modified time", "skip asset");
-            return;
-        }
+        .map_err(RenderingConfigurationAssetProcessError::SpawnCompilerProcessFailed)?
+        .wait()
+        .map_err(RenderingConfigurationAssetProcessError::ProcessWaitFailed)?;
 
-        let img = match image::open(&args.source_path) {
-            Ok(x) => x,
-            Err(e) => {
-                tracing::error!(reason = ?e, "Failed to open asset");
-                std::process::exit(1);
-            }
-        };
+        Ok(())
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ImageAssetProcessError {
+    #[error("Failed to open asset: {0}")]
+    OpenFailed(image::ImageError),
+    #[error("Failed to create ktx2 texture: {0:?}")]
+    CreateKtx2Failed(ktx::Error),
+    #[error("Ktx2 Operation Failure({0}): {1:?}")]
+    Ktx2OperationFailure(&'static str, ktx::Error),
+    #[error("invalid utf-8 sequence in out path")]
+    InvalidOutPath,
+    #[error("Nul byte in out path")]
+    NulByteInOutPath(std::ffi::NulError),
+}
+
+pub struct ImageAssetProcessor;
+impl AssetProcessor for ImageAssetProcessor {
+    fn can_process(&self, source_path: &Path) -> bool {
+        source_path
+            .extension()
+            .is_some_and(|x| x == "png" || x == "jpg" || x == "tiff")
+    }
+
+    fn dest_path(&self, source_file_name: &OsStr, out_dir_path: &Path) -> PathBuf {
+        out_dir_path
+            .join(source_file_name)
+            .with_extension("pa1-texture2d")
+    }
+
+    fn process(
+        &self,
+        source_path: &Path,
+        out_path: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let img = image::open(source_path).map_err(ImageAssetProcessError::OpenFailed)?;
 
         let mut ktx = ktx::Texture2::new(
             &ktx::ffi::ktxTextureCreateInfo {
@@ -114,62 +121,127 @@ fn main() {
             },
             true,
         )
-        .expect("failed to initialize ktxTexture2");
+        .map_err(ImageAssetProcessError::CreateKtx2Failed)?;
         ktx.set_image_from_memory(0, 0, 0, img.to_rgba8().as_raw())
-            .expect("ktx.set_image_from_memory failed");
+            .map_err(|e| {
+                ImageAssetProcessError::Ktx2OperationFailure("set_image_from_memory", e)
+            })?;
         ktx.compress_basis_ex(
             &mut ktx::BasisParams::new()
                 .uastc()
                 .uastc_flags(ktx::ffi::KTX_PACK_UASTC_LEVEL_DEFAULT)
                 .uastc_rdo(),
         )
-        .expect("ktx.compress_basis_ex failed");
-        ktx.deflate_zstd(11).expect("ktx.deflate_zstd failed");
+        .map_err(|e| ImageAssetProcessError::Ktx2OperationFailure("compress_basis_ex", e))?;
+        ktx.deflate_zstd(11)
+            .map_err(|e| ImageAssetProcessError::Ktx2OperationFailure("deflate_zstd", e))?;
         ktx.write_to_named_file(
-            &std::ffi::CString::new(dest_path.to_str().expect("invalid utf-8 seq"))
-                .expect("invalid cstr seq"),
-        )
-        .expect("ktx.write_to_named_file failed");
-    } else if ext.is_some_and(|x| x == "wav" || x == "mp3" || x == "ogg" || x == "flac") {
-        // sound asset(TODO: convert to what?)
-        let dest_path = args
-            .out_dir
-            .as_deref()
-            .unwrap_or_else(|| args.source_path.parent().expect("no parent?"))
-            .join(
-                args.source_path
-                    .file_name()
-                    .expect("no file name in source path"),
+            &std::ffi::CString::new(
+                out_path
+                    .to_str()
+                    .ok_or(ImageAssetProcessError::InvalidOutPath)?,
             )
-            .with_extension("pa1-audio");
-        if !args.force_rebuild
-            && let (Ok(x), Ok(y)) = (args.source_path.metadata(), dest_path.metadata())
-            && x.modified().unwrap() <= y.modified().unwrap()
-        {
-            tracing::info!(reason = "modified time", "skip asset");
-            return;
-        }
+            .map_err(ImageAssetProcessError::NulByteInOutPath)?,
+        )
+        .map_err(|e| ImageAssetProcessError::Ktx2OperationFailure("write_to_named_file", e))?;
 
-        if let Err(e) = std::fs::copy(&args.source_path, &dest_path) {
-            tracing::error!(reason = ?e, "Failed to copy asset file");
-            std::process::exit(1);
-        }
-    } else {
+        Ok(())
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum SoundAssetProcessError {
+    #[error("Failed to copy asset file: {0}")]
+    CopyFailed(std::io::Error),
+}
+
+pub struct SoundAssetProcessor;
+impl AssetProcessor for SoundAssetProcessor {
+    fn can_process(&self, source_path: &Path) -> bool {
+        source_path
+            .extension()
+            .is_some_and(|x| x == "wav" || x == "mp3" || x == "ogg" || x == "flac")
+    }
+
+    fn dest_path(&self, source_file_name: &OsStr, out_dir_path: &Path) -> PathBuf {
+        out_dir_path
+            .join(source_file_name)
+            .with_extension("pa1-audio")
+    }
+
+    fn process(
+        &self,
+        source_path: &Path,
+        out_path: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        // TODO: convert to what?
+        std::fs::copy(source_path, out_path).map_err(SoundAssetProcessError::CopyFailed)?;
+
+        Ok(())
+    }
+}
+
+fn main() {
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    let args = Args::parse();
+    let (Some(source_dir), Some(source_file_name)) =
+        (args.source_path.parent(), args.source_path.file_name())
+    else {
+        tracing::error!(
+            path = %args.source_path.display(),
+            "invalid source file path provided"
+        );
+        std::process::exit(1);
+    };
+    let dest_dir = args.out_dir.as_deref().unwrap_or(source_dir);
+
+    let span = tracing::span!(
+        tracing::Level::INFO,
+        "main",
+        source_path = %args.source_path.display(),
+    );
+    let _span_enter = span.enter();
+
+    let processors: [Box<dyn AssetProcessor>; _] = [
+        Box::new(RenderingConfigurationAssetProcessor),
+        Box::new(ImageAssetProcessor),
+        Box::new(SoundAssetProcessor),
+    ];
+    let mut matching_processors_iter = processors
+        .iter()
+        .filter(|x| x.can_process(&args.source_path));
+    let Some(processor) = matching_processors_iter.next() else {
         // unknown assets
         tracing::warn!("found unknown assets(not processed)");
-        let dest_path = args
-            .out_dir
-            .as_deref()
-            .unwrap_or_else(|| args.source_path.parent().expect("no parent?"))
-            .join(
-                args.source_path
-                    .file_name()
-                    .expect("no file name in source path"),
-            );
+        let dest_path = dest_dir.join(source_file_name);
 
         if let Err(e) = std::fs::copy(&args.source_path, &dest_path) {
             tracing::error!(reason = ?e, "Failed to copy asset file");
             std::process::exit(1);
         }
+
+        return;
+    };
+    if matching_processors_iter.next().is_some() {
+        tracing::error!("Cannot determine an asset processor");
+        std::process::exit(1);
+    }
+
+    let dest_path = processor.dest_path(source_file_name, dest_dir);
+    if !args.force_rebuild
+        && let (Ok(x), Ok(y)) = (args.source_path.metadata(), dest_path.metadata())
+        && x.modified().unwrap() <= y.modified().unwrap()
+    {
+        tracing::info!(reason = "modified time", "skip asset");
+        return;
+    }
+
+    if let Err(e) = processor.process(&args.source_path, &dest_path) {
+        tracing::error!(reason = ?e, "Failed to process asset");
+        std::process::exit(1);
     }
 }
