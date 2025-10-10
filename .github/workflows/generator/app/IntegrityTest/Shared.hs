d95e332b430e0e4b464f68e251727a5b4bdf8598
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2025-10-10 11:31:13 +0900

    fix: installing ccache

--- .github/workflows/generator/app/IntegrityTest/Shared.hs
+++ .github/workflows/generator/app/IntegrityTest/Shared.hs
@@ -111,11 +111,12 @@ checkTools precondition = reportJobFailure $ GHA.namedAs "Tools" $ GHA.job steps
               rustCacheStep,
               thirdpartySubmodulesCacheStep,
               setupCargoOutputTranslatorStep,
+              GHA.runStep "sudo apt-get update && sudo apt-get install ccache",
               GHA.runStep "ccache --show-config",
-              GHA.namedAs "Pre-build native deps(slang)" $
+              GHA.namedAs "Pre-build c deps(slang)" $
                 GHA.runStep "cmake --preset default -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache && cmake --build --preset releaseWithDebugInfo -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache --target slang --target slang-glslang --target slang-glsl-module"
                   & GHA.workAt "thirdparty/slang/source-repo",
-              GHA.namedAs "Pre-build native deps(ktx)" $
+              GHA.namedAs "Pre-build c deps(ktx)" $
                 GHA.runStep "cmake . -B build -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache && cmake --build build -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache --target ktx"
                   & GHA.workAt "thirdparty/ktx/source-repo",
               GHA.namedAs "check" $
