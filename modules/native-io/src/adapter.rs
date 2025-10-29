bac0a6b157899e9a500db111b6367992f7935108
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2025-10-29 13:39:35 +0900

    add: async read/seek adapter

--- modules/native-io/src/adapter.rs
+++ modules/native-io/src/adapter.rs
@@ -1,5 +1,6 @@
 //! Adapter structs for other crate's traits.
 
+use pin_project::pin_project;
 use std::io::{Read, Seek};
 
 pub struct RandomBlobReadSeekAdapter<R> {
@@ -39,5 +40,119 @@ where
         };
         self.pos = new_pos;
         Ok(new_pos)
+    }
+}
+
+#[pin_project(project = RandomBlobAsyncReadSeekAdapterStateProjected)]
+enum RandomBlobAsyncReadSeekAdapterState<'r, R: crate::RandomReadBlobAsync + 'r> {
+    Idle,
+    Reading(#[pin] R::ReadFuture<'r, 'static>),
+    ReadingVec(#[pin] R::ReadVecFuture<'r, 'static, 'static>),
+}
+
+#[pin_project]
+pub struct RandomBlobAsyncReadSeekAdapter<'r, R: crate::RandomReadBlobAsync + 'r> {
+    inner: &'r R,
+    pos: u64,
+    #[pin]
+    state: RandomBlobAsyncReadSeekAdapterState<'r, R>,
+}
+impl<'r, R: crate::RandomReadBlobAsync + 'r> RandomBlobAsyncReadSeekAdapter<'r, R> {
+    #[inline(always)]
+    pub const fn new(inner: &'r R) -> Self {
+        Self {
+            inner,
+            pos: 0,
+            state: RandomBlobAsyncReadSeekAdapterState::Idle,
+        }
+    }
+}
+impl<'r, R: crate::RandomReadBlobAsync + 'r> futures_io::AsyncRead
+    for RandomBlobAsyncReadSeekAdapter<'r, R>
+{
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut [u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        let mut this = self.project();
+
+        loop {
+            match this.state.as_mut().project() {
+                RandomBlobAsyncReadSeekAdapterStateProjected::Idle => {
+                    this.state.set(RandomBlobAsyncReadSeekAdapterState::Reading(
+                        this.inner.read_async(*this.pos, unsafe {
+                            core::mem::transmute::<&mut [_], &mut [core::mem::MaybeUninit<_>]>(buf)
+                        }),
+                    ));
+                }
+                RandomBlobAsyncReadSeekAdapterStateProjected::Reading(f) => {
+                    let r = std::task::ready!(f.poll(cx));
+                    this.state.set(RandomBlobAsyncReadSeekAdapterState::Idle);
+                    break std::task::Poll::Ready(r);
+                }
+                RandomBlobAsyncReadSeekAdapterStateProjected::ReadingVec(_) => {
+                    panic!("poll_read called but poll_read_vectored is ongoing");
+                }
+            }
+        }
+    }
+
+    fn poll_read_vectored(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        bufs: &mut [std::io::IoSliceMut<'_>],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        let mut this = self.project();
+
+        loop {
+            match this.state.as_mut().project() {
+                RandomBlobAsyncReadSeekAdapterStateProjected::Idle => {
+                    this.state
+                        .set(RandomBlobAsyncReadSeekAdapterState::ReadingVec(
+                            this.inner.readv_async(*this.pos, unsafe {
+                                core::mem::transmute::<&mut [_], &mut [_]>(&mut *bufs)
+                            }),
+                        ));
+                }
+                RandomBlobAsyncReadSeekAdapterStateProjected::ReadingVec(f) => {
+                    let r = std::task::ready!(f.poll(cx));
+                    this.state.set(RandomBlobAsyncReadSeekAdapterState::Idle);
+                    break std::task::Poll::Ready(r);
+                }
+                RandomBlobAsyncReadSeekAdapterStateProjected::Reading(_) => {
+                    panic!("poll_read_vectored called but poll_read is ongoing");
+                }
+            }
+        }
+    }
+}
+impl<'r, R: crate::RandomReadBlobAsync + 'r> futures_io::AsyncSeek
+    for RandomBlobAsyncReadSeekAdapter<'r, R>
+{
+    fn poll_seek(
+        self: std::pin::Pin<&mut Self>,
+        _cx: &mut std::task::Context<'_>,
+        pos: std::io::SeekFrom,
+    ) -> std::task::Poll<std::io::Result<u64>> {
+        // これは同期的にできる
+        let pref = self.project().pos;
+        *pref = match pos {
+            std::io::SeekFrom::Start(x) => x,
+            std::io::SeekFrom::Current(x) => pref.checked_add_signed(x).ok_or_else(|| {
+                std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "resulting file pointer is out of range!",
+                )
+            })?,
+            std::io::SeekFrom::End(x) => pref.checked_sub_signed(x).ok_or_else(|| {
+                std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "resulting file pointer is out of range!",
+                )
+            })?,
+        };
+
+        std::task::Poll::Ready(Ok(*pref))
     }
 }
