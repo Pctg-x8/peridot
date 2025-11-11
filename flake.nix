2bb4deb6f23cc05e2c7c57b145a31e90b12ad591
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2025-11-11 23:57:35 +0900

    add: basic nix devenv

--- flake.nix
+++ flake.nix
@@ -2,9 +2,34 @@
   description = "Peridot devenv";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, ... }: {
-
-  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      build-tools = pkgs.writeShellApplication {
+        name = "build-tools";
+        runtimeInputs = [
+          pkgs.rustup
+          # for buidling cdeps
+          pkgs.cmake
+          pkgs.ninja
+        ];
+        text = "cd tools; cargo build; cd ..";
+      };
+    in
+    {
+      devShells."${system}".default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustup
+          # for building cdeps
+          pkgs.cmake
+          pkgs.ninja
+          # helper scripts
+          build-tools
+        ];
+      };
+    };
 }
